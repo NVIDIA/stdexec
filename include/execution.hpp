c10f7ae45595cf52d68756bb2e18a3e21ed99171
Eric Niebler <eniebler@boost.org>
2021-08-20 16:30:30 -0700

    license and copyright

--- include/execution.hpp
+++ include/execution.hpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) NVIDIA
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
 #include <stdexcept>
@@ -359,6 +374,19 @@ namespace std::execution {
         return __impl::__sender<__id_t<remove_cvref_t<S>>, F>{(S&&)s, (F&&)f};
       }
     } lazy_then {};
+
+    inline constexpr struct then_t {
+      template<sender S, class F>
+        requires tag_invocable<then_t, S, F>
+      sender auto operator()(S&& s, F f) const
+        noexcept(nothrow_tag_invocable<then_t, S, F>) {
+        return tag_invoke(then_t{}, (S&&) s, (F&&) f);
+      }
+      template<sender S, class F>
+      sender auto operator()(S&& s, F f) const {
+        return lazy_then((S&&) s, (F&&) f);
+      }
+    } then {};
   }
 
   // Make the lazy_then sender typed if the input sender is also typed.
