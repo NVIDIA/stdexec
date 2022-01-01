4633f8216b1bf50c8699b604f83c7d10b7fe25f7
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2022-01-01 18:26:49 +0200

    Add a build system based on conan/cmake
    Goals:
    - ensure proper compilation of examples (i.e., changes to execution.hpp
    would lead to recompilation of the examples)
    - allow adding unit tests with ease
    - allow depending on external libraries (e.g., Catch2 for unit tests)
    - allow making CI jobs with ease

---
+++ test/test_include.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) Lucian Radu Teodorescu
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <catch2/catch.hpp>
+#include <execution.hpp>
+
+TEST_CASE("Simple test for just", "[sender_algo]") {
+  auto snd = std::execution::just(13);
+  static_cast<void>(snd);
+  REQUIRE_FALSE(snd.sends_done);
+}
