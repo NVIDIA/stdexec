d73e9fda0f02eb30061954e38da92f86a5a1c8c6
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2022-01-03 19:19:14 +0200

    Add tests for sender factories

---
+++ test/algos/factories/test_transfer_just.cpp
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) Lucian Radu Teodorescu
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <catch2/catch.hpp>
+#include <execution.hpp>
+#include <test_common/schedulers.hpp>
+#include <test_common/receivers.hpp>
+#include <test_common/type_helpers.hpp>
+
+namespace ex = std::execution;
+
+TEST_CASE("transfer_just returns a sender", "[factories][transfer_just]") {
+  auto snd = ex::transfer_just(inline_scheduler{}, 13);
+  static_assert(ex::sender<decltype(snd)>);
+  (void)snd;
+}
+TEST_CASE("transfer_just returns a typed_sender", "[factories][transfer_just]") {
+  auto snd = ex::transfer_just(inline_scheduler{}, 13);
+  static_assert(ex::typed_sender<decltype(snd)>);
+  (void)snd;
+}
+TEST_CASE("transfer_just simple example", "[factories][transfer_just]") {
+  inline_scheduler sched;
+  auto snd = ex::transfer_just(sched, 13);
+  auto op = ex::connect(std::move(snd), expect_value_receiver{13});
+  ex::start(op);
+  // The receiver checks if we receive the right value
+}
+
+TEST_CASE(
+    "transfer_just calls the receiver when the scheduler dictates", "[factories][transfer_just]") {
+  int recv_value{0};
+  impulse_scheduler sched;
+  auto snd = ex::transfer_just(sched, 13);
+  auto op = ex::connect(snd, expect_value_receiver_ex{&recv_value});
+  ex::start(op);
+  // Up until this point, the scheduler didn't start any task; no effect expected
+  CHECK(recv_value == 0);
+
+  // Tell the scheduler to start executing one task
+  sched.start_next();
+  CHECK(recv_value == 13);
+}
+TEST_CASE("transfer_just can be called with value type scheduler", "[factories][transfer_just]") {
+  auto snd = ex::transfer_just(inline_scheduler{}, 13);
+  auto op = ex::connect(std::move(snd), expect_value_receiver{13});
+  ex::start(op);
+  // The receiver checks if we receive the right value
+}
+TEST_CASE("transfer_just can be called with rvalue ref scheduler", "[factories][transfer_just]") {
+  auto snd = ex::transfer_just(inline_scheduler{}, 13);
+  auto op = ex::connect(std::move(snd), expect_value_receiver{13});
+  ex::start(op);
+  // The receiver checks if we receive the right value
+}
+TEST_CASE("transfer_just can be called with const ref scheduler", "[factories][transfer_just]") {
+  const inline_scheduler sched;
+  auto snd = ex::transfer_just(sched, 13);
+  auto op = ex::connect(std::move(snd), expect_value_receiver{13});
+  ex::start(op);
+  // The receiver checks if we receive the right value
+}
+TEST_CASE("transfer_just can be called with ref scheduler", "[factories][transfer_just]") {
+  inline_scheduler sched;
+  auto snd = ex::transfer_just(sched, 13);
+  auto op = ex::connect(std::move(snd), expect_value_receiver{13});
+  ex::start(op);
+  // The receiver checks if we receive the right value
+}
+
+TEST_CASE("transfer_just forwards set_error calls", "[factories][transfer_just]") {
+  error_scheduler<std::exception_ptr> sched{std::exception_ptr{}};
+  auto snd = ex::transfer_just(sched, 13);
+  auto op = ex::connect(std::move(snd), expect_error_receiver{});
+  ex::start(op);
+  // The receiver checks if we receive an error
+}
+TEST_CASE("transfer_just forwards set_error calls of other types", "[factories][transfer_just]") {
+  error_scheduler<std::string> sched{std::string{"error"}};
+  auto snd = ex::transfer_just(sched, 13);
+  auto op = ex::connect(std::move(snd), expect_error_receiver{});
+  ex::start(op);
+  // The receiver checks if we receive an error
+}
+TEST_CASE("transfer_just forwards set_done calls", "[factories][transfer_just]") {
+  done_scheduler sched{};
+  auto snd = ex::transfer_just(sched, 13);
+  auto op = ex::connect(std::move(snd), expect_done_receiver{});
+  ex::start(op);
+  // The receiver checks if we receive the done signal
+}
+
+TEST_CASE("transfer_just has the values_type corresponding to the given values",
+    "[factories][transfer_just]") {
+  inline_scheduler sched{};
+
+  check_val_types<type_array<type_array<int>>>(ex::transfer_just(sched, 1));
+  check_val_types<type_array<type_array<int, double>>>(ex::transfer_just(sched, 3, 0.14));
+  check_val_types<type_array<type_array<int, double, std::string>>>(
+      ex::transfer_just(sched, 3, 0.14, std::string{"pi"}));
+}
+TEST_CASE("transfer_just keeps error_types from scheduler's sender", "[factories][transfer_just]") {
+  inline_scheduler sched1{};
+  error_scheduler sched2{};
+  error_scheduler<int> sched3{43};
+
+  check_err_types<type_array<std::exception_ptr>>(ex::transfer_just(sched1, 1));
+  check_err_types<type_array<std::exception_ptr>>(ex::transfer_just(sched2, 2));
+  // check_err_types<type_array<int, std::exception_ptr>>(ex::transfer_just(sched3, 3));
+  // TODO: transfer_just should also forward the error types sent by the scheduler's sender
+  // incorrect check:
+  check_err_types<type_array<std::exception_ptr>>(ex::transfer_just(sched3, 3));
+}
+TEST_CASE("transfer_just keeps send_done from scheduler's sender", "[factories][transfer_just]") {
+  inline_scheduler sched1{};
+  error_scheduler sched2{};
+  done_scheduler sched3{};
+
+  check_sends_done<false>(ex::transfer_just(sched1, 1));
+  check_sends_done<false>(ex::transfer_just(sched2, 2));
+  // check_sends_done<true>(ex::transfer_just(sched3, 3));
+  // TODO: transfer_just should forward its "sends_done" info
+  // incorrect check:
+  check_sends_done<false>(ex::transfer_just(sched3, 3));
+}
+
+TEST_CASE("transfer_just advertises its completion scheduler", "[factories][transfer_just]") {
+  inline_scheduler sched1{};
+  error_scheduler sched2{};
+  done_scheduler sched3{};
+
+  REQUIRE(ex::get_completion_scheduler<ex::set_value_t>(ex::transfer_just(sched1, 1)) == sched1);
+  REQUIRE(ex::get_completion_scheduler<ex::set_error_t>(ex::transfer_just(sched1, 1)) == sched1);
+  REQUIRE(ex::get_completion_scheduler<ex::set_done_t>(ex::transfer_just(sched1, 1)) == sched1);
+
+  REQUIRE(ex::get_completion_scheduler<ex::set_value_t>(ex::transfer_just(sched2, 2)) == sched2);
+  REQUIRE(ex::get_completion_scheduler<ex::set_error_t>(ex::transfer_just(sched2, 2)) == sched2);
+  REQUIRE(ex::get_completion_scheduler<ex::set_done_t>(ex::transfer_just(sched2, 2)) == sched2);
+
+  REQUIRE(ex::get_completion_scheduler<ex::set_value_t>(ex::transfer_just(sched3, 3)) == sched3);
+  REQUIRE(ex::get_completion_scheduler<ex::set_error_t>(ex::transfer_just(sched3, 3)) == sched3);
+  REQUIRE(ex::get_completion_scheduler<ex::set_done_t>(ex::transfer_just(sched3, 3)) == sched3);
+}
+
+// Modify the value when we invoke this custom defined transfer_just implementation
+auto tag_invoke(decltype(ex::transfer_just), inline_scheduler sched, std::string value) {
+  return ex::transfer(ex::just("Hello, " + value), sched);
+}
+
+TEST_CASE("transfer_just can be customized", "[factories][transfer_just]") {
+  // The customization will alter the value passed in
+  auto snd = ex::transfer_just(inline_scheduler{}, std::string{"world"});
+  std::string res;
+  auto op = ex::connect(std::move(snd), expect_value_receiver_ex<std::string>(&res));
+  ex::start(op);
+  REQUIRE(res == "Hello, world");
+}
---
+++ test/test_common/schedulers.hpp
@@ -0,0 +1,212 @@
+/*
+ * Copyright (c) Lucian Radu Teodorescu
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <execution.hpp>
+
+#include <functional>
+#include <vector>
+
+namespace ex = std::execution;
+
+//! Scheduler that will send impulses on user's request.
+//! One can obtain senders from this, connect them to receivers and start the operation states.
+//! Until the scheduler is told to start the next operation, the actions in the operation states are
+//! not executed. This is similar to a task scheduler, but it's single threaded. Doesn't have any
+//! thread safety built in.
+struct impulse_scheduler {
+  private:
+  //! Command type that can store the action of firing up a sender
+  using oper_command_t = std::function<void()>;
+  using cmd_vec_t = std::vector<oper_command_t>;
+  //! All the commands that we need to somehow
+  std::shared_ptr<cmd_vec_t> all_commands_{};
+
+  template <typename R>
+  struct oper {
+    cmd_vec_t* all_commands_;
+    R receiver_;
+
+    oper(cmd_vec_t* all_commands, R&& recv)
+        : all_commands_(all_commands)
+        , receiver_((R &&) recv) {}
+
+    friend void tag_invoke(ex::start_t, oper& self) noexcept {
+      // Enqueue another command to the list of all commands
+      // The scheduler will start this, whenever start_next() is called
+      self.all_commands_->emplace_back([recv = (R &&) self.receiver_]() {
+        try {
+          ex::set_value((R &&) recv);
+        } catch (...) {
+          ex::set_error((R &&) recv, std::current_exception());
+        }
+      });
+    }
+  };
+
+  struct my_sender {
+    cmd_vec_t* all_commands_;
+
+    template <template <class...> class Tuple, template <class...> class Variant>
+    using value_types = Variant<Tuple<>>;
+    template <template <class...> class Variant>
+    using error_types = Variant<std::exception_ptr>;
+    static constexpr bool sends_done = true;
+
+    template <typename R>
+    friend oper<R> tag_invoke(ex::connect_t, my_sender self, R&& r) {
+      return {self.all_commands_, (R &&) r};
+    }
+
+    friend impulse_scheduler tag_invoke(
+        ex::get_completion_scheduler_t<ex::set_value_t>, my_sender) {
+      return {};
+    }
+  };
+
+  public:
+  impulse_scheduler()
+      : all_commands_(std::make_shared<cmd_vec_t>(cmd_vec_t{})) {}
+  ~impulse_scheduler() = default;
+
+  //! Actually start the command from the last started operation_state
+  void start_next() {
+    if (!all_commands_->empty()) {
+      // Pop one command from the queue
+      auto cmd = std::move(all_commands_->front());
+      all_commands_->erase(all_commands_->begin());
+      // Execute the command, i.e., send an impulse to the connected sender
+      cmd();
+    }
+  }
+
+  friend my_sender tag_invoke(ex::schedule_t, const impulse_scheduler& self) {
+    return my_sender{self.all_commands_.get()};
+  }
+
+  friend bool operator==(impulse_scheduler, impulse_scheduler) noexcept { return true; }
+  friend bool operator!=(impulse_scheduler, impulse_scheduler) noexcept { return false; }
+};
+
+//! Scheduler that executes everything inline, i.e., on the same thread
+struct inline_scheduler {
+  template <typename R>
+  struct oper {
+    R recv_;
+    friend void tag_invoke(ex::start_t, oper& self) noexcept {
+      try {
+        ex::set_value((R &&) self.recv_);
+      } catch (...) {
+        ex::set_error((R &&) self.recv_, std::current_exception());
+      }
+    }
+  };
+
+  struct my_sender {
+    template <template <class...> class Tuple, template <class...> class Variant>
+    using value_types = Variant<Tuple<>>;
+    template <template <class...> class Variant>
+    using error_types = Variant<std::exception_ptr>;
+    static constexpr bool sends_done = false;
+
+    template <typename R>
+    friend oper<R> tag_invoke(ex::connect_t, my_sender self, R&& r) {
+      return {(R &&) r};
+    }
+
+    friend inline_scheduler tag_invoke(ex::get_completion_scheduler_t<ex::set_value_t>, my_sender) {
+      return {};
+    }
+  };
+
+  friend my_sender tag_invoke(ex::schedule_t, inline_scheduler) { return {}; }
+
+  friend bool operator==(inline_scheduler, inline_scheduler) noexcept { return true; }
+  friend bool operator!=(inline_scheduler, inline_scheduler) noexcept { return false; }
+};
+
+//! Scheduler that returns a sender that always completes with error.
+template <typename E = std::exception_ptr>
+struct error_scheduler {
+  template <typename R>
+  struct oper {
+    R recv_;
+    E err_;
+
+    friend void tag_invoke(ex::start_t, oper& self) noexcept {
+      ex::set_error((R &&) self.recv_, self.err_);
+    }
+  };
+
+  struct my_sender {
+    E err_;
+
+    template <template <class...> class Tuple, template <class...> class Variant>
+    using value_types = Variant<Tuple<>>;
+    template <template <class...> class Variant>
+    using error_types = Variant<E>;
+    static constexpr bool sends_done = false;
+
+    template <typename R>
+    friend oper<R> tag_invoke(ex::connect_t, my_sender self, R&& r) {
+      return {(R &&) r, (E &&) self.err_};
+    }
+
+    friend error_scheduler tag_invoke(ex::get_completion_scheduler_t<ex::set_value_t>, my_sender) {
+      return {};
+    }
+  };
+
+  E err_;
+
+  friend my_sender tag_invoke(ex::schedule_t, error_scheduler self) { return {(E &&) self.err_}; }
+
+  friend bool operator==(error_scheduler, error_scheduler) noexcept { return true; }
+  friend bool operator!=(error_scheduler, error_scheduler) noexcept { return false; }
+};
+
+//! Scheduler that returns a sender that always completes with cancellation.
+struct done_scheduler {
+  template <typename R>
+  struct oper {
+    R recv_;
+    friend void tag_invoke(ex::start_t, oper& self) noexcept { ex::set_done((R &&) self.recv_); }
+  };
+
+  struct my_sender {
+    template <template <class...> class Tuple, template <class...> class Variant>
+    using value_types = Variant<Tuple<>>;
+    template <template <class...> class Variant>
+    using error_types = Variant<>;
+    static constexpr bool sends_done = true;
+
+    template <typename R>
+    friend oper<R> tag_invoke(ex::connect_t, my_sender self, R&& r) {
+      return {(R &&) r};
+    }
+
+    template <typename CPO>
+    friend done_scheduler tag_invoke(ex::get_completion_scheduler_t<CPO>, my_sender) {
+      return {};
+    }
+  };
+
+  friend my_sender tag_invoke(ex::schedule_t, done_scheduler) { return {}; }
+
+  friend bool operator==(done_scheduler, done_scheduler) noexcept { return true; }
+  friend bool operator!=(done_scheduler, done_scheduler) noexcept { return false; }
+};
