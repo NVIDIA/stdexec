f25e0694dff41f29792876a552b9cc4f0a40f538
Eric Niebler <eniebler@nvidia.com>
2022-06-14 15:52:37 -0700

    port sender creation utility, `create<completions...>(lambda)`, from libunifex

--- include/execution.hpp
+++ include/execution.hpp
@@ -230,7 +230,7 @@ namespace std::execution {
         using __count_of =
           integral_constant<
             size_t,
-            (__mapply<__mcount, __signal_args_t<_Sigs, _Tag>>::value + ...)>;
+            (__mapply<__mcount, __signal_args_t<_Sigs, _Tag>>::value + ... + 0u)>;
 
       template <template <class...> class _Tuple, template <class...> class _Variant>
         using __value_types =
@@ -4290,5 +4290,79 @@ namespace std::this_thread {
   using __sync_wait::sync_wait_with_variant_t;
   inline constexpr sync_wait_with_variant_t sync_wait_with_variant{};
 } // namespace std::this_thread
+
+// Things that are not yet part of a proposal:
+namespace std::execution::extra {
+  namespace __create {
+    struct __void {
+      template <class _Fn>
+        void emplace(_Fn&& __fn) noexcept(__nothrow_callable<_Fn>) {
+          ((_Fn&&) __fn)();
+        }
+    };
+
+    template <class _Receiver, class _Args>
+      struct __context {
+        [[no_unique_address]] _Receiver receiver;
+        [[no_unique_address]] _Args args;
+      };
+
+    template <class _ReceiverId, class _FnId, class _ArgsId>
+      struct __operation {
+        using _Fn = __t<_FnId>;
+        using _Context = __context<__t<_ReceiverId>, __t<_ArgsId>>;
+        using _Result = __call_result_t<_Fn, _Context&>;
+        using _State = __if_c<same_as<_Result, void>, __void, optional<_Result>>;
+
+        [[no_unique_address]] _Context __ctx_;
+        [[no_unique_address]] _Fn __fn_;
+        [[no_unique_address]] _State __state_{};
+
+        friend void tag_invoke(start_t, __operation& __self) noexcept {
+          __self.__state_.emplace(__conv{
+            [&]() noexcept {
+              return ((_Fn&&) __self.__fn_)(__self.__ctx_);
+            }
+          });
+        }
+      };
+
+    template <class _Sigs, class _FnId, class _ArgsId>
+      struct __sender {
+        using _Fn = __t<_FnId>;
+        using _Args = __t<_ArgsId>;
+        using completion_signatures = _Sigs;
+
+        _Fn __fn_;
+        _Args __args_;
+
+        template <__decays_to<__sender> _Self, receiver_of<_Sigs> _Receiver>
+          requires __callable<_Fn, __context<_Receiver, _Args>&> &&
+            constructible_from<_Fn, __member_t<_Self, _Fn>> &&
+            constructible_from<_Args, __member_t<_Self, _Args>>
+        friend auto tag_invoke(connect_t, _Self&& __self, _Receiver&& __rcvr)
+          -> __operation<__x<decay_t<_Receiver>>, _FnId, _ArgsId> {
+          static_assert(__nothrow_callable<_Fn, __context<_Receiver, _Args>&>);
+          return {{(_Receiver&&) __rcvr, ((_Self&&) __self).__args_}, ((_Self&&) __self).__fn_};
+        }
+      };
+
+    template <class... _Sigs>
+      struct __create_t {
+        using __compl_sigs = completion_signatures<_Sigs...>;
+
+        template <class _Fn, class... _Args>
+            requires move_constructible<_Fn> &&
+              constructible_from<__decayed_tuple<_Args...>, _Args...>
+          auto operator()(_Fn __fn, _Args&&... __args) const
+            -> __sender<__compl_sigs, __x<_Fn>, __x<__decayed_tuple<_Args...>>> {
+            return {(_Fn&&) __fn, {(_Args&&) __args...}};
+          }
+      };
+  } // namespace __create
+
+  template <class... _Sigs>
+    inline constexpr __create::__create_t<_Sigs...> create {};
+} // namespace std::execution::extra
 
 _PRAGMA_POP()
