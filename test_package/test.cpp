530175f99dd1a5b5acd6a3a3851c0005d5f0814c
Paul M. Bendixen <paulbendixen@gmail.com>
2022-08-24 23:46:37 +0200

    Conan: Fix test package
    Fix after changing sync_wait from `std` to `_P2300`
    
    Closes #579

--- test_package/test.cpp
+++ test_package/test.cpp
@@ -4,6 +4,6 @@ int main()
 {
 	auto x = std::execution::just(42);
 
-	auto [a] = std::this_thread::sync_wait(std::move(x)).value();
+	auto [a] = _P2300::this_thread::sync_wait(std::move(x)).value();
 	return (a==42)?0:-1;
 }
