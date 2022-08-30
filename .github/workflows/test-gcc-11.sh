22cd712536af79ce51a3a17de66e5ea71af16d64
ptaylor <paul.e.taylor@me.com>
2022-08-29 21:03:49 -0700

    remove conan from github workflow scripts

--- .github/workflows/test-gcc-11.sh
+++ .github/workflows/test-gcc-11.sh
@@ -15,7 +15,6 @@ mkdir -p "${CURDIR}/../../${BUILDDIR}"
 # Run docker with action-cxx-toolkit to check our code
 docker run ${DOCKER_RUN_PARAMS} \
     -e INPUT_BUILDDIR="/github/workspace/${BUILDDIR}" \
-    -e INPUT_PREBUILD_COMMAND="conan config set storage.path=/github/workspace/${BUILDDIR}/.conan" \
     -e INPUT_MAKEFLAGS='-j 4' \
     -e INPUT_CC='gcc-11' \
     -e INPUT_CHECKS='build test install warnings' lucteo/action-cxx-toolkit.gcc11:latest
