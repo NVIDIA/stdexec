f236fba40e22cd182ee867eeec2803e8c9aee44e
ptaylor <paul.e.taylor@me.com>
2022-08-30 00:07:44 -0700

    update CI workflows and test scripts to install git + update CMake version before running

--- .github/workflows/test-gcc-11.sh
+++ .github/workflows/test-gcc-11.sh
@@ -17,6 +17,10 @@ docker run ${DOCKER_RUN_PARAMS} \
     -e INPUT_BUILDDIR="/github/workspace/${BUILDDIR}" \
     -e INPUT_MAKEFLAGS='-j 4' \
     -e INPUT_CC='gcc-11' \
-    -e INPUT_CHECKS='build test install warnings' lucteo/action-cxx-toolkit.gcc11:latest
+    -e INPUT_CHECKS='build test install' \
+    -e INPUT_PREBUILD_COMMAND="\
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | apt-key add -; \
+apt update && apt install -y --no-install-recommends git cmake" \
+    lucteo/action-cxx-toolkit.gcc11:latest
 status=$?
 printStatus $status
