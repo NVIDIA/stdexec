3f63f0b0b9a1b0ecacc2f00099a74fa6ccebd2c2
ptaylor <paul.e.taylor@me.com>
2022-09-06 20:19:59 -0700

    initial ensure_started implementation

--- test/algos/adaptors/test_ensure_started.cpp
+++ test/algos/adaptors/test_ensure_started.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) Lucian Radu Teodorescu
+ * Copyright (c) NVIDIA
  *
  * Licensed under the Apache License Version 2.0 with LLVM Exceptions
  * (the "License"); you may not use this file except in compliance with
@@ -15,30 +16,47 @@
  */
 
 #include <catch2/catch.hpp>
+
 #include <execution.hpp>
+#include <async_scope.hpp>
 #include <test_common/schedulers.hpp>
 #include <test_common/receivers.hpp>
 #include <test_common/type_helpers.hpp>
 
 namespace ex = std::execution;
+using _P2519::execution::async_scope;
 
 // TODO: implement ensure_started
-// TEST_CASE("ensure_started returns a sender", "[adaptors][ensure_started]") {
-//   auto snd = ex::ensure_started(ex::just(19));
-//   static_assert(ex::sender<decltype(snd)>);
-//   (void)snd;
-// }
-// TEST_CASE("ensure_started with environment returns a sender", "[adaptors][ensure_started]") {
-//   auto snd = ex::ensure_started(ex::just(19), 8, [](int idx, int val) {});
-//   static_assert(ex::sender<decltype(snd), empty_env>);
-//   (void)snd;
-// }
-// TEST_CASE("ensure_started simple example", "[adaptors][ensure_started]") {
-//   bool called{false};
-//   auto snd1 = ex::just() | ex::then([&] { called = true; });
-//   CHECK_FALSE(called);
-//   auto snd = ex::ensure_started(std::move(snd1));
-//   CHECK(called);
-//   auto op = ex::connect(std::move(snd), expect_void_receiver{});
-//   ex::start(op);
-// }
+TEST_CASE("ensure_started returns a sender", "[adaptors][ensure_started]") {
+  auto snd = ex::ensure_started(ex::just(19));
+  static_assert(ex::sender<decltype(snd)>);
+  (void)snd;
+}
+
+TEST_CASE("ensure_started with environment returns a sender", "[adaptors][ensure_started]") {
+  auto snd = ex::ensure_started(ex::just(19));
+  static_assert(ex::sender<decltype(snd), empty_env>);
+  (void)snd;
+}
+
+TEST_CASE("ensure_started simple example", "[adaptors][ensure_started]") {
+  bool called{false};
+  auto snd1 = ex::just() | ex::then([&] { called = true; });
+  CHECK_FALSE(called);
+  auto snd = ex::ensure_started(std::move(snd1));
+  CHECK(called);
+  auto op = ex::connect(std::move(snd), expect_void_receiver{});
+  ex::start(op);
+}
+
+TEST_CASE("stopping ensure_started before the source completes calls set_stopped", "[adaptors][ensure_started]") {
+  async_scope scope;
+  impulse_scheduler sch;
+  auto snd = ex::on(sch, ex::just(19)) | ex::ensure_started();
+  auto op = ex::connect(std::move(snd), expect_stopped_receiver_ex{});
+  ex::start(op);
+  // request stop before the source yields a value
+  scope.get_stop_source().request_stop();
+  // make the source yield the value
+  sch.start_next();
+}
