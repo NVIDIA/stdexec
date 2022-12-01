0edcfbda8044587fb0203e39a81a9e569d69e2ee
Eric Niebler <eniebler@nvidia.com>
2022-12-01 13:43:21 -0800

    Remove coroutine workaround for nvhpc
    For a time, nvidia's hpc compiler, nvc++, was erroneously claiming to support coroutines. That bug has been fixed in 22.11, which is now the earliest supported nvc++ version. Remove the workaround as it's no longer needed.

--- include/stdexec/coroutine.hpp
+++ include/stdexec/coroutine.hpp
@@ -18,8 +18,7 @@
 #include "concepts.hpp"
 
 #include <version>
-#if __cpp_impl_coroutine >= 201902 && __cpp_lib_coroutine >= 201902 \
-  && !STDEXEC_NVHPC()
+#if __cpp_impl_coroutine >= 201902 && __cpp_lib_coroutine >= 201902
 #include <coroutine>
 namespace __coro = std;
 #elif defined(__cpp_coroutines) && __has_include(<experimental/coroutine>)
