60f782b05e1cb39d49ef526589c461a5b51c09dc
Maikel Nadolski <maikel.nadolski@gmail.com>
2023-02-11 07:44:03 +0100

    Fix formatting

--- include/exec/when_any.hpp
+++ include/exec/when_any.hpp
@@ -61,9 +61,8 @@ namespace exec
                   __completion_signatures_t<_Env, _SenderIds...>>;
 
 
-    template <class _Receiver, class _ResultType>
+    template <class _Receiver, class _ResultVariant>
       struct __op_base : __immovable {
-
         __op_base(_Receiver&& __receiver, int __n_senders)
         : __count_{__n_senders}
         , __receiver_{(_Receiver&&) __receiver} {
@@ -81,7 +80,7 @@ namespace exec
         std::atomic<int> __count_{};
 
         _Receiver __receiver_;
-        _ResultType __result_{};
+        _ResultVariant __result_{};
 
         template <class _CPO, class... _Args>
           void notify(_CPO, _Args&&... __args) noexcept {
@@ -115,24 +114,24 @@ namespace exec
                           (_Tuple &&) __result);
                     }
                   },
-                  (_ResultType &&) __result_);
+                  (_ResultVariant &&) __result_);
             }
           }
       };
 
-    template <class _Receiver, class _ResultType> 
+    template <class _Receiver, class _ResultVariant> 
       struct __receiver {
         class __t {
         public:
           using __id = __receiver;
 
-          explicit __t(__op_base<_Receiver, _ResultType>* __op) noexcept : __op_{__op} {}
+          explicit __t(__op_base<_Receiver, _ResultVariant>* __op) noexcept : __op_{__op} {}
 
         private:
-          __op_base<_Receiver, _ResultType>* __op_;
+          __op_base<_Receiver, _ResultVariant>* __op_;
 
           template <__one_of<set_value_t, set_error_t, set_stopped_t> _CPO, class... _Args>
-              requires requires (_ResultType& result, _Args&&... __args) {
+              requires requires (_ResultVariant& result, _Args&&... __args) {
                 { result.template emplace<std::tuple<_CPO, std::decay_t<_Args>...>>(_CPO{}, (_Args&&) __args...) };
               }
             friend void tag_invoke(_CPO, __t&& __self, _Args&&... __args) noexcept {
@@ -142,7 +141,7 @@ namespace exec
           friend __env_t<env_of_t<_Receiver>> tag_invoke(get_env_t, const __t& __self) noexcept {
             using __with_token = __with<get_stop_token_t, in_place_stop_token>;
             auto __token = __with_token{__self.__op_->__stop_source_.get_token()};
-            return __make_env(stdexec::get_env(__self.__op_->__receiver_), (__with_token &&) __token);
+            return __make_env(get_env(__self.__op_->__receiver_), (__with_token &&) __token);
           }
         };
       };
@@ -158,21 +157,21 @@ namespace exec
         class __t : __op_base_t {
          public:
           template <class _SendersTuple>
-          __t(_SendersTuple&& __senders, _Receiver&& __rcvr)
-          : __t{(_SendersTuple&&) __senders, (_Receiver &&) __rcvr,
-                std::index_sequence_for<_SenderIds...>{}} {}
+            __t(_SendersTuple&& __senders, _Receiver&& __rcvr)
+            : __t{(_SendersTuple&&) __senders, (_Receiver &&) __rcvr,
+                  std::index_sequence_for<_SenderIds...>{}} {}
 
          private:
 
 
           template <class _SenderTuple, std::size_t... _Is>
-          __t(_SenderTuple&& __senders, _Receiver&& __rcvr,
-              std::index_sequence<_Is...>)
-          : __op_base_t{(_Receiver&&) __rcvr, static_cast<int>(sizeof...(_SenderIds))}
-          , __ops_{__conv{[&__senders, this] {
-              return connect(std::get<_Is>((_SenderTuple&&) __senders), 
-                             __receiver_t{static_cast<__op_base_t*>(this)});
-            }}...} {}
+            __t(_SenderTuple&& __senders, _Receiver&& __rcvr,
+                std::index_sequence<_Is...>)
+            : __op_base_t{(_Receiver&&) __rcvr, static_cast<int>(sizeof...(_SenderIds))}
+            , __ops_{__conv{[&__senders, this] {
+                return connect(std::get<_Is>((_SenderTuple&&) __senders), 
+                              __receiver_t{static_cast<__op_base_t*>(this)});
+              }}...} {}
 
           std::tuple<connect_result_t<stdexec::__t<_SenderIds>, __receiver_t>...> __ops_;
 
@@ -180,7 +179,7 @@ namespace exec
             __self.__on_stop_.emplace(get_stop_token(get_env(__self.__receiver_)),
                                       __on_stop_requested{__self.__stop_source_});
             if (__self.__stop_source_.stop_requested()) {
-              stdexec::set_stopped((_Receiver&&) __self.__receiver_);
+              set_stopped((_Receiver&&) __self.__receiver_);
             } else {
               std::apply([](auto&... __ops) { (start(__ops), ...); }, __self.__ops_);
             }
@@ -233,8 +232,8 @@ namespace exec
       template <class... _Senders>
         using __sender_t = __t<__sender<__id<decay_t<_Senders>>...>>;
 
-      template <stdexec::sender... _Senders>
-          requires (sizeof...(_Senders) > 0 && stdexec::sender<__sender_t<_Senders...>>)
+      template <sender... _Senders>
+          requires (sizeof...(_Senders) > 0 && sender<__sender_t<_Senders...>>)
         __sender_t<_Senders...> operator()(_Senders&&... __senders) const
             noexcept((__nothrow_decay_copyable<_Senders> && ...)) {
           return __sender_t<_Senders...>((_Senders&&) __senders...);
