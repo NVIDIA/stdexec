41cdfd69f0bf9dd72d27a0982d4b0d64b4d7ff3f
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2023-02-28 12:04:07 -0800

    Re-add proper error handling and other fixes to rebase the new CUDA `reduce` adaptor on the latest version of `stdexec`.

--- include/nvexec/stream/reduce.cuh
+++ include/nvexec/stream/reduce.cuh
@@ -109,201 +109,234 @@ template <class BoundRange, class BoundFun, class... As>
 
 template <class SenderId, class ReceiverId, class BoundRange, class BoundFun>
     requires ((ranges::range<BoundRange> || !is_bound<BoundRange>) && stdexec::__movable_value<BoundFun>)
-  class receiver_t : public receiver_base_t {
-    using Sender = stdexec::__t<SenderId>;
-    using Receiver = stdexec::__t<ReceiverId>;
+  struct receiver_t {
+    class __t : public stream_receiver_base {
+      using Sender = stdexec::__t<SenderId>;
+      using Receiver = stdexec::__t<ReceiverId>;
 
-    template <class... As>
-      using result_size_for_t = stdexec::__index<sizeof(reduce_result_t<BoundRange, BoundFun, As...>)>;
+      template <class... As>
+        struct result_size_for {
+          using __t = stdexec::__msize_t<sizeof(reduce_result_t<BoundRange, BoundFun, As...>)>;
+        };
 
-    template <class... Sizes>
-      struct max_in_pack {
-        static constexpr std::size_t value = std::max({std::size_t{}, stdexec::__v<Sizes>...});
+      template <class... Sizes>
+        struct max_in_pack {
+          static constexpr std::size_t value = std::max({std::size_t{}, stdexec::__v<Sizes>...});
+        };
+
+      struct max_result_size {
+        template <class... _As>
+          using result_size_for_t = stdexec::__t<result_size_for<_As...>>;
+
+        static constexpr std::size_t value =
+          stdexec::__v<
+            stdexec::__gather_completions_for<
+              stdexec::set_value_t,
+              Sender,
+              stdexec::env_of_t<Receiver>,
+              stdexec::__q<result_size_for_t>,
+              stdexec::__q<max_in_pack>>>;
       };
 
-    struct max_result_size {
-      static constexpr std::size_t value =
-        stdexec::__v<
-          stdexec::__gather_sigs_t<
-            stdexec::set_value_t,
-            Sender,
-            stdexec::env_of_t<Receiver>,
-            stdexec::__q<result_size_for_t>,
-            stdexec::__q<max_in_pack>>>;
-    };
+      operation_state_base_t<ReceiverId>& op_state_;
+      [[no_unique_address]] BoundRange rng_;
+      [[no_unique_address]] BoundFun fun_;
 
-    [[no_unique_address]] BoundRange rng_;
-    [[no_unique_address]] BoundFun fun_;
-    operation_state_base_t<ReceiverId>& op_state_;
+    public:
+      using __id = receiver_t;
 
-  public:
+      constexpr static std::size_t memory_allocation_size = max_result_size::value;
 
-    constexpr static std::size_t memory_allocation_size = max_result_size::value;
+      // Range and function are bound.
+      friend void tag_invoke(stdexec::set_value_t, __t&& self) noexcept
+        requires (is_bound<BoundRange> && is_bound<BoundFun>) {
+          using Result = reduce_result_t<BoundRange, BoundFun>;
+          self.template launch<Result>(self.rng_, self.fun_);
+        }
 
-    // Range and function are bound.
-    friend void tag_invoke(stdexec::set_value_t, receiver_t&& self) noexcept
-      requires (is_bound<BoundRange> && is_bound<BoundFun>) {
-        using Result = reduce_result_t<BoundRange, BoundFun>;
-        self.launch<Result>(self.rng_, self.fun_);
-      }
+      // Range is bound, function is the default.
+      friend void tag_invoke(stdexec::set_value_t, __t&& self) noexcept
+        requires (is_bound<BoundRange> && !is_bound<BoundFun>) {
+          using Result = reduce_result_t<BoundRange, BoundFun>;
+          self.template launch<Result>(self.rng_, cub::Sum{});
+        }
 
-    // Range is bound, function is the default.
-    friend void tag_invoke(stdexec::set_value_t, receiver_t&& self) noexcept
-      requires (is_bound<BoundRange> && !is_bound<BoundFun>) {
-        using Result = reduce_result_t<BoundRange, BoundFun>;
-        self.launch<Result>(self.rng_, cub::Sum{});
-      }
+      // Range is bound, predecessor sends function.
+      template <stdexec::__movable_value Fun>
+          requires (is_bound<BoundRange>)
+        friend void tag_invoke(stdexec::set_value_t, __t&& self, Fun&& fun) noexcept {
+          static_assert(!is_bound<BoundFun>);
+          using Result = reduce_result_t<BoundRange, BoundFun, Fun>;
+          self.template launch<Result>(self.rng_, (Fun&&) fun);
+        }
 
-    // Range is bound, predecessor sends function.
-    template <stdexec::__movable_value Fun>
-        requires (is_bound<BoundRange>)
-      friend void tag_invoke(stdexec::set_value_t, receiver_t&& self, Fun&& fun) noexcept {
-        static_assert(!is_bound<BoundFun>);
-        using Result = reduce_result_t<BoundRange, BoundFun, Fun>;
-        self.launch<Result>(self.rng_, (Fun&&) fun);
-      }
+      // Predecessor sends range, function is bound.
+      template <ranges::range Range>
+          requires (is_bound<BoundFun>)
+        friend void tag_invoke(stdexec::set_value_t, __t&& self, Range&& rng) noexcept {
+          static_assert(!is_bound<BoundRange>);
+          using Result = reduce_result_t<BoundRange, BoundFun, Range>;
+          self.template launch<Result>((Range&&) rng, self.fun_);
+        }
 
-    // Predecessor sends range, function is bound.
-    template <ranges::range Range>
-        requires (is_bound<BoundFun>)
-      friend void tag_invoke(stdexec::set_value_t, receiver_t&& self, Range&& rng) noexcept {
-        static_assert(!is_bound<BoundRange>);
-        using Result = reduce_result_t<BoundRange, BoundFun, Range>;
-        self.launch<Result>((Range&&) rng, self.fun_);
-      }
+      // Predecessor sends range, function is the default.
+      template <ranges::range Range>
+          requires (!is_bound<BoundFun>)
+        friend void tag_invoke(stdexec::set_value_t, __t&& self, Range&& rng) noexcept {
+          static_assert(!is_bound<BoundRange>);
+          using Result = reduce_result_t<BoundRange, BoundFun, Range>;
+          self.template launch<Result>((Range&&) rng, cub::Sum{});
+        }
 
-    // Predecessor sends range, function is the default.
-    template <ranges::range Range>
-        requires (!is_bound<BoundFun>)
-      friend void tag_invoke(stdexec::set_value_t, receiver_t&& self, Range&& rng) noexcept {
-        static_assert(!is_bound<BoundRange>);
-        using Result = reduce_result_t<BoundRange, BoundFun, Range>;
-        self.launch<Result>((Range&&) rng, cub::Sum{});
-      }
+      // Predecessor sends range and function.
+      template <ranges::range Range, stdexec::__movable_value Fun>
+        friend void tag_invoke(stdexec::set_value_t, __t&& self, Range&& rng, Fun&& fun) noexcept {
+          static_assert(!is_bound<BoundRange> && !is_bound<BoundFun>);
+          using Result = reduce_result_t<BoundRange, BoundFun, Range, Fun>;
+          self.template launch<Result>((Range&&) rng, (Fun&&) fun);
+        }
 
-    // Predecessor sends range and function.
-    template <ranges::range Range, stdexec::__movable_value Fun>
-      friend void tag_invoke(stdexec::set_value_t, receiver_t&& self, Range&& rng, Fun&& fun) noexcept {
-        static_assert(!is_bound<BoundRange> && !is_bound<BoundFun>);
-        using Result = reduce_result_t<BoundRange, BoundFun, Range, Fun>;
-        self.launch<Result>((Range&&) rng, (Fun&&) fun);
-      }
+      template <class Result, ranges::range Range, stdexec::__movable_value Fun = cub::Sum>
+        void launch(Range&& rng, Fun&& fun = {}) noexcept {
+          cudaStream_t stream = op_state_.get_stream();
 
-    template <class Result, ranges::range Range, stdexec::__movable_value Fun = cub::Sum>
-      void launch(Range&& rng, Fun&& fun = {}) noexcept {
-        cudaStream_t stream = op_state_.stream_;
+          Result *d_out = reinterpret_cast<Result*>(op_state_.temp_storage_);
 
-        Result *d_out = reinterpret_cast<Result*>(op_state_.temp_storage_);
+          void *d_temp_storage{};
+          std::size_t temp_storage_size{};
 
-        void *d_temp_storage{};
-        std::size_t temp_storage_size{};
+          auto first = ranges::begin(rng);
 
-        auto first = ranges::begin(rng);
-        auto last = ranges::end(rng);
+          std::size_t num_items = ranges::size(rng);
 
-        std::size_t num_items = ranges::size(rng);
+          std::cout << "size: " << num_items << std::endl;
 
-        THROW_ON_CUDA_ERROR(cub::DeviceReduce::Reduce(d_temp_storage, temp_storage_size, first,
-                                                      d_out, num_items, fun, Result{},
-                                                      stream));
-        THROW_ON_CUDA_ERROR(cudaMallocAsync(&d_temp_storage, temp_storage_size, stream));
+          std::abort();
 
-        THROW_ON_CUDA_ERROR(cub::DeviceReduce::Reduce(d_temp_storage, temp_storage_size, first,
-                                                      d_out, num_items, fun, Result{},
-                                                      stream));
-        THROW_ON_CUDA_ERROR(cudaFreeAsync(d_temp_storage, stream));
+          cudaError_t status;
 
-        op_state_.propagate_completion_signal(stdexec::set_value, *d_out);
-      }
+          do {
+            if (status = STDEXEC_DBG_ERR(cub::DeviceReduce::Reduce(
+                  d_temp_storage, temp_storage_size,
+                  first, d_out, num_items,
+                  fun, Result{},
+                  stream));
+                status != cudaSuccess) {
+              break;
+            }
 
-    template <stdexec::__one_of<stdexec::set_error_t,
-                            stdexec::set_stopped_t> Tag,
-              class... As _NVCXX_CAPTURE_PACK(As)>
-      friend void tag_invoke(Tag tag, receiver_t&& self, As&&... as) noexcept {
-        _NVCXX_EXPAND_PACK(As, as,
+            if (status = STDEXEC_DBG_ERR(
+                  cudaMallocAsync(&d_temp_storage, temp_storage_size, stream));
+                status != cudaSuccess) {
+              break;
+            }
+
+            if (status = STDEXEC_DBG_ERR(cub::DeviceReduce::Reduce(
+                  d_temp_storage, temp_storage_size,
+                  first, d_out, num_items,
+                  fun, Result{},
+                  stream));
+                status != cudaSuccess) {
+              break;
+            }
+
+            status = STDEXEC_DBG_ERR(cudaFreeAsync(d_temp_storage, stream));
+          } while (false);
+
+          if (status == cudaSuccess) {
+            op_state_.propagate_completion_signal(stdexec::set_value, *d_out);
+          } else {
+            op_state_.propagate_completion_signal(stdexec::set_error, std::move(status));
+          }
+        }
+
+      template <stdexec::__one_of<stdexec::set_error_t, stdexec::set_stopped_t> Tag,
+                class... As>
+        friend void tag_invoke(Tag tag, __t&& self, As&&... as) noexcept {
           self.op_state_.propagate_completion_signal(tag, (As&&)as...);
-        );
+        }
+
+      friend stdexec::env_of_t<Receiver> tag_invoke(stdexec::get_env_t, const __t& self) {
+        return stdexec::get_env(self.op_state_.receiver_);
       }
 
-    friend stdexec::env_of_t<Receiver> tag_invoke(stdexec::get_env_t, const receiver_t& self) {
-      return stdexec::get_env(self.op_state_.receiver_);
-    }
-
-    receiver_t(operation_state_base_t<ReceiverId>& op_state, BoundRange rng, BoundFun fun)
-      : rng_((BoundRange&&) rng)
-      , fun_((BoundFun&&) fun)
-      , op_state_(op_state)
-    {}
+      __t(operation_state_base_t<ReceiverId>& op_state, BoundRange rng, BoundFun fun)
+        : op_state_(op_state)
+        , rng_((BoundRange&&) rng)
+        , fun_((BoundFun&&) fun)
+      {}
+    };
   };
 
 } // namespace reduce_
 
-template <class SenderId, class BoundRangeId, class BoundFunId>
-  struct reduce_sender_t : gpu_sender_base_t {
-    using Sender = stdexec::__t<SenderId>;
-    using BoundRange = stdexec::__t<BoundRangeId>;
-    using BoundFun = stdexec::__t<BoundFunId>;
+template <class SenderId, class BoundRange, class BoundFun>
+  struct reduce_sender_t {
+    struct __t : stream_sender_base {
+      using __id = reduce_sender_t;
 
-    Sender sndr_;
-    [[no_unique_address]] BoundRange rng_;
-    [[no_unique_address]] BoundFun fun_;
+      using Sender = stdexec::__t<SenderId>;
 
-    template <class Receiver>
-      using receiver_t = reduce_::receiver_t<SenderId, stdexec::__x<Receiver>, BoundRange, BoundFun>;
+      Sender sndr_;
+      [[no_unique_address]] BoundRange rng_;
+      [[no_unique_address]] BoundFun fun_;
 
-    template <class... As>
-      using set_value_t =
-        stdexec::completion_signatures<stdexec::set_value_t(
-          std::add_lvalue_reference_t<
-            reduce_::reduce_result_t<BoundRange, BoundFun, As...>
-          >
-        )>;
+      template <class Receiver>
+        using receiver_t =
+          stdexec::__t<reduce_::receiver_t<SenderId, stdexec::__id<Receiver>, BoundRange, BoundFun>>;
 
-    template <class Self, class Env>
-      using completion_signatures =
-        stdexec::make_completion_signatures<
-          stdexec::__member_t<Self, Sender>,
-          Env,
-          stdexec::completion_signatures<stdexec::set_error_t(cudaError_t)>,
-          set_value_t
-          >;
+      template <class... As>
+        using set_value_t =
+          stdexec::completion_signatures<stdexec::set_value_t(
+            std::add_lvalue_reference_t<
+              reduce_::reduce_result_t<BoundRange, BoundFun, As...>
+            >
+          )>;
 
-    template <stdexec::__decays_to<reduce_sender_t> Self, stdexec::receiver Receiver>
-      requires stdexec::receiver_of<Receiver, completion_signatures<Self, stdexec::env_of_t<Receiver>>>
-    friend auto tag_invoke(stdexec::connect_t, Self&& self, Receiver&& rcvr)
-      -> stream_op_state_t<stdexec::__member_t<Self, Sender>, receiver_t<Receiver>, Receiver> {
-        return stream_op_state<stdexec::__member_t<Self, Sender>>(
-          ((Self&&)self).sndr_,
-          (Receiver&&)rcvr,
-          [&](operation_state_base_t<stdexec::__x<Receiver>>& stream_provider) -> receiver_t<Receiver> {
-            return receiver_t<Receiver>(stream_provider, self.rng_, self.fun_);
-          });
-    }
+      template <class Self, class Env>
+        using completion_signatures =
+          stdexec::make_completion_signatures<
+            stdexec::__copy_cvref_t<Self, Sender>,
+            Env,
+            stdexec::completion_signatures<stdexec::set_error_t(cudaError_t)>,
+            set_value_t
+            >;
 
-    template <stdexec::__decays_to<reduce_sender_t> Self, class Env>
-    friend auto tag_invoke(stdexec::get_completion_signatures_t, Self&&, Env)
-      -> stdexec::dependent_completion_signatures<Env>;
+      template <stdexec::__decays_to<__t> Self, stdexec::receiver Receiver>
+        requires stdexec::receiver_of<Receiver, completion_signatures<Self, stdexec::env_of_t<Receiver>>>
+      friend auto tag_invoke(stdexec::connect_t, Self&& self, Receiver&& rcvr)
+        -> stream_op_state_t<stdexec::__copy_cvref_t<Self, Sender>, receiver_t<Receiver>, Receiver> {
+          return stream_op_state<stdexec::__copy_cvref_t<Self, Sender>>(
+            ((Self&&)self).sndr_,
+            (Receiver&&)rcvr,
+            [&](operation_state_base_t<stdexec::__id<Receiver>>& stream_provider) -> receiver_t<Receiver> {
+              return receiver_t<Receiver>(stream_provider, self.rng_, self.fun_);
+            });
+      }
 
-    template <stdexec::__decays_to<reduce_sender_t> Self, class Env>
-    friend auto tag_invoke(stdexec::get_completion_signatures_t, Self&&, Env)
-      -> completion_signatures<Self, Env> requires true;
+      template <stdexec::__decays_to<__t> Self, class Env>
+      friend auto tag_invoke(stdexec::get_completion_signatures_t, Self&&, Env)
+        -> stdexec::dependent_completion_signatures<Env>;
 
-    template <stdexec::tag_category<stdexec::forwarding_sender_query> Tag, class... As>
-      requires stdexec::__callable<Tag, const Sender&, As...>
-    friend auto tag_invoke(Tag tag, const reduce_sender_t& self, As&&... as)
-      noexcept(stdexec::__nothrow_callable<Tag, const Sender&, As...>)
-      -> stdexec::__call_result_if_t<stdexec::tag_category<Tag, stdexec::forwarding_sender_query>, Tag, const Sender&, As...> {
-      return ((Tag&&) tag)(self.sndr_, (As&&) as...);
-    }
+      template <stdexec::__decays_to<__t> Self, class Env>
+      friend auto tag_invoke(stdexec::get_completion_signatures_t, Self&&, Env)
+        -> completion_signatures<Self, Env> requires true;
+
+      friend auto tag_invoke(stdexec::get_env_t, const __t& self)
+        noexcept(stdexec::__nothrow_callable<stdexec::get_env_t, const Sender&>)
+          -> stdexec::__call_result_t<stdexec::get_env_t, const Sender&> {
+        return stdexec::get_env(self.sndr_);
+      }
+    };
   };
 
 struct reduce_t {
   template <class Sender, class BoundRange, class BoundFun>
-    using sender_t = reduce_sender_t<
-      stdexec::__x<::cuda::std::remove_cvref_t<Sender>>,
-      stdexec::__x<::cuda::std::remove_cvref_t<BoundRange>>,
-      stdexec::__x<::cuda::std::remove_cvref_t<BoundFun>>
-    >;
+    using sender_t = stdexec::__t<reduce_sender_t<
+      stdexec::__id<::cuda::std::decay_t<Sender>>,
+      ::cuda::std::remove_cvref_t<BoundRange>,
+      ::cuda::std::remove_cvref_t<BoundFun>
+    >>;
 
   // Range and function are bound.
   template <stdexec::sender Sender, ranges::range Range, stdexec::__movable_value Fun>
