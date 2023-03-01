5c79a2dde61f7dbf8cfbddb985b556b4f4f17d5d
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2023-03-01 11:24:08 -0800

    Don't use a namespace alias in the reduce example; we don't need it.

--- examples/nvexec/reduce.cpp
+++ examples/nvexec/reduce.cpp
@@ -3,17 +3,15 @@
 #include <nvexec/stream_context.cuh>
 #include <stdexec/execution.hpp>
 
-namespace ex = stdexec;
-
 int main() {
   std::vector<int> input(2048, 1);
 
   nvexec::stream_context stream{};
 
-  auto snd = ex::transfer_just(stream.get_scheduler(), input)
+  auto snd = stdexec::transfer_just(stream.get_scheduler(), input)
            | nvexec::reduce();
 
-  auto [result] = ex::sync_wait(std::move(snd)).value();
+  auto [result] = stdexec::sync_wait(std::move(snd)).value();
 
   std::cout << "result: " << result << std::endl;
 }
