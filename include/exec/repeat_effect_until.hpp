72aef85f9d059e78e3660fa089f93bbb93aec7f3
Eric Niebler <eniebler@boost.org>
2023-03-25 11:11:43 -0700

    Update include/exec/repeat_effect_until.hpp

--- include/exec/repeat_effect_until.hpp
+++ include/exec/repeat_effect_until.hpp
@@ -43,7 +43,7 @@ namespace exec {
         using __id = __operation;
         using __receiver_t = stdexec::__t<__receiver<_SourceId, _ReceiverId>>;
         using __source_on_scheduler_sender =
-          __call_result_t<stdexec::on_t, trampoline_scheduler, _Source>;
+          __call_result_t<stdexec::on_t, trampoline_scheduler, _Source&>;
         using __source_op_t = stdexec::connect_result_t<__source_on_scheduler_sender, __receiver_t>;
 
         [[no_unique_address]] _Source __source_;
