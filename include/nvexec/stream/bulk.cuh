5f2f06a1fdd377c4c49ebfcc41e4f48a5b09b814
Mikael Simberg <simberg@cscs.ch>
2023-04-04 12:51:54 +0200

    Correctly check that stream bulk callable takes references to sent values

--- include/nvexec/stream/bulk.cuh
+++ include/nvexec/stream/bulk.cuh
@@ -48,7 +48,7 @@ namespace nvexec::STDEXEC_STREAM_DETAIL_NS {
 
         template <class... As>
         friend void tag_invoke(stdexec::set_value_t, __t&& self, As&&... as) noexcept
-          requires stdexec::__callable<Fun, Shape, As...>
+          requires stdexec::__callable<Fun, Shape, As&...>
         {
           operation_state_base_t<ReceiverId>& op_state = self.op_state_;
 
@@ -187,7 +187,7 @@ namespace nvexec::STDEXEC_STREAM_DETAIL_NS {
 
         template <class... As>
         friend void tag_invoke(stdexec::set_value_t, __t&& self, As&&... as) noexcept
-          requires stdexec::__callable<Fun, Shape, As...>
+          requires stdexec::__callable<Fun, Shape, As&...>
         {
           operation_t<CvrefSenderId, ReceiverId, Shape, Fun>& op_state = self.op_state_;
 
