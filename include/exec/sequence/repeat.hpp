616481ba8ae8bc41ca4125c9dc6f51dda07aaf42
Maikel Nadolski <maikel.nadolski@gmail.com>
2023-04-13 14:53:04 +0200

    Apply Erics review on repeat

--- include/exec/sequence/repeat.hpp
+++ include/exec/sequence/repeat.hpp
@@ -24,85 +24,101 @@ namespace exec {
   namespace __repeat {
     using namespace stdexec;
 
+    template <class _ReceiverId>
+    struct __op_base {
+      [[no_unique_address]] __t<_ReceiverId> __rcvr_;
+      void (*__repeat_)(__op_base*) noexcept = nullptr;
+    };
+
+    template <class _ReceiverId>
+    struct __receiver {
+      using _Receiver = stdexec::__t<_ReceiverId>;
+
+      struct __t {
+        using __id = __receiver;
+        __op_base<_ReceiverId>* __op_;
+
+        template <same_as<set_value_t> _SetValue, same_as<__t> _Self>
+        friend void tag_invoke(_SetValue, _Self&& __self) noexcept {
+          STDEXEC_ASSERT(__self.__op_->__repeat_);
+          __self.__op_->__repeat_(__self.__op_);
+        }
+
+        template <same_as<set_error_t> _SetError, same_as<__t> _Self, class _Error>
+          requires __callable<set_error_t, _Receiver&&, _Error>
+        friend void tag_invoke(_SetError, _Self&& __self, _Error e) noexcept {
+          stdexec::set_error(
+            static_cast<_Receiver&&>(__self.__op_->__rcvr_), static_cast<_Error&&>(e));
+        }
+
+        template <same_as<set_stopped_t> _SetStopped, same_as<__t> _Self>
+        friend void tag_invoke(_SetStopped, _Self&& __self) noexcept {
+          auto token = stdexec::get_stop_token(stdexec::get_env(__self.__op_->__rcvr_));
+          if (token.stop_requested()) {
+            stdexec::set_stopped(static_cast<_Receiver&&>(__self.__op_->__rcvr_));
+          } else {
+            stdexec::set_value(static_cast<_Receiver&&>(__self.__op_->__rcvr_));
+          }
+        }
+
+        friend env_of_t<_Receiver> tag_invoke(get_env_t, const __t& __self) noexcept(
+          __nothrow_callable<get_env_t, const _Receiver&>) {
+          return stdexec::get_env(__self.__op_->__rcvr_);
+        }
+      };
+    };
+
     // Takes a sender and creates a sequence sender by repeating the sender as item to the set_next
     // operation.
-    template <class _SourceSenderId, class _ReceiverId>
+    template <class _SourceSender, class _ReceiverId>
     struct __operation {
-      using _SourceSender = stdexec::__t<_SourceSenderId>;
       using _Receiver = stdexec::__t<_ReceiverId>;
       using __next_sender_t = __call_result_t<set_next_t, _Receiver&, _SourceSender>;
       using __next_on_scheduler_sender =
         __call_result_t<on_t, exec::trampoline_scheduler, __next_sender_t&&>;
 
-      struct __t;
+      using __receiver_t = stdexec::__t<__receiver<_ReceiverId>>;
 
-      struct __receiver {
-        __t* __op_;
-
-        friend void tag_invoke(set_value_t, __receiver&& __self) noexcept {
-          __self.__op_->repeat();
-        }
-
-        template <__decays_to<__receiver> _Self, class _Error>
-          requires __callable<set_error_t, _Receiver&&, _Error>
-        friend void tag_invoke(set_error_t, _Self&& __self, _Error e) noexcept {
-          stdexec::set_error((_Receiver&&) __self.__op_->__rcvr_, (_Error&&) e);
-        }
-
-        friend void tag_invoke(set_stopped_t, __receiver&& __self) noexcept {
-          auto token = stdexec::get_stop_token(stdexec::get_env(__self.__op_->__rcvr_));
-          if (token.stop_requested()) {
-            stdexec::set_stopped((_Receiver&&) __self.__op_->__rcvr_);
-          } else {
-            stdexec::set_value((_Receiver&&) __self.__op_->__rcvr_);
-          }
-        }
-
-        friend env_of_t<_Receiver> tag_invoke(get_env_t, const __receiver& __self) noexcept(
-          __nothrow_callable<get_env_t, const _Receiver&>) {
-          return stdexec::get_env(__self.__op_->__rcvr_);
-        }
-      };
-
-      struct __t {
+      struct __t : __op_base<_ReceiverId> {
         using __id = __operation;
-        [[no_unique_address]] _Receiver __rcvr_;
         [[no_unique_address]] _SourceSender __source_;
         exec::trampoline_scheduler __trampoline_;
-        std::optional<connect_result_t<__next_on_scheduler_sender, __receiver>> __next_op_;
+        std::optional<connect_result_t<__next_on_scheduler_sender, __receiver_t>> __next_op_;
 
-        void repeat() noexcept {
-          auto __token = stdexec::get_stop_token(stdexec::get_env(__rcvr_));
+        static void __repeat(__op_base<_ReceiverId>* __base) noexcept {
+          __t* __self = static_cast<__t*>(__base);
+          auto __token = stdexec::get_stop_token(stdexec::get_env(__self->__rcvr_));
           if (__token.stop_requested()) {
-            stdexec::set_stopped((_Receiver&&) __rcvr_);
+            stdexec::set_stopped((_Receiver&&) __self->__rcvr_);
             return;
           }
           try {
-            auto& __next = __next_op_.emplace(__conv{[&] {
+            auto& __next = __self->__next_op_.emplace(__conv{[&] {
               return connect(
-                stdexec::on(__trampoline_, set_next(__rcvr_, _SourceSender{__source_})),
-                __receiver{this});
+                stdexec::on(
+                  __self->__trampoline_, exec::set_next(__self->__rcvr_, __self->__source_)),
+                __receiver_t{__base});
             }});
             stdexec::start(__next);
           } catch (...) {
-            stdexec::set_error((_Receiver&&) __rcvr_, std::current_exception());
+            stdexec::set_error((_Receiver&&) __self->__rcvr_, std::current_exception());
           }
         }
 
         friend void tag_invoke(start_t, __t& __self) noexcept {
-          __self.repeat();
+          __repeat(&__self);
         }
 
         template <__decays_to<_SourceSender> _Sndr, __decays_to<_Receiver> _Rcvr>
         explicit __t(_Sndr&& __source, _Rcvr&& __rcvr)
-          : __rcvr_{(_Rcvr&&) __rcvr}
+          : __op_base<_ReceiverId>{(_Rcvr&&) __rcvr, &__repeat}
           , __source_{(_Sndr&&) __source} {
         }
       };
     };
 
     template <class _SourceSender, class _Receiver>
-    using __operation_t = __t<__operation<__id<_SourceSender>, __id<__decay_t<_Receiver>>>>;
+    using __operation_t = __t<__operation<_SourceSender, __id<__decay_t<_Receiver>>>>;
 
     template <class _Source, class _Env>
     using __compl_sigs = make_completion_signatures<
@@ -122,15 +138,15 @@ namespace exec {
         __call_result_t<on_t, exec::trampoline_scheduler, __next_sender<_Rcvr>&&>;
 
       template <class _Rcvr>
-      using __recveiver = typename __operation<__id<_Source>, __id<__decay_t<_Rcvr>>>::__receiver;
+      using __recveiver_t = stdexec::__t<__receiver<__id<__decay_t<_Rcvr>>>>;
 
       class __t {
         [[no_unique_address]] _Source __source_;
 
         template <__decays_to<__t> _Self, class _Receiver>
-          requires sequence_receiver_of<_Receiver, __compl_sigs<_Source&&, env_of_t<_Receiver>>>
+          requires sequence_receiver_of<_Receiver, __compl_sigs<_Source&, env_of_t<_Receiver>>>
                 && __valid<__next_on_scheduler_sender, _Receiver>
-                && sender_to<__next_on_scheduler_sender<_Receiver>, __recveiver<_Receiver>>
+                && sender_to<__next_on_scheduler_sender<_Receiver>, __recveiver_t<_Receiver>>
         friend auto tag_invoke(sequence_connect_t, _Self&& __self, _Receiver&& __rcvr)
           -> __operation_t<_Source, _Receiver> {
           return __operation_t<_Source, _Receiver>{
@@ -139,7 +155,7 @@ namespace exec {
 
         template <__decays_to<__t> _Self, class _Env>
         friend auto tag_invoke(get_completion_signatures_t, _Self&&, const _Env&)
-          -> __compl_sigs<_Source&&, _Env>;
+          -> __compl_sigs<_Source&, _Env>;
 
        public:
         using __id = __sender;
