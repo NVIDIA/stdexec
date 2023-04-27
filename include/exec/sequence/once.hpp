5a341fe5958df0ebe52d234faf1fc1e87dd6ade0
Maikel Nadolski <maikel.nadolski@gmail.com>
2023-04-27 12:51:45 +0200

    Implement once and tests

---
+++ include/exec/sequence/once.hpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2023 Maikel Nadolski
+ * Copyright (c) 2023 NVIDIA Corporation
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "../sequence_senders.hpp"
+
+namespace exec {
+  namespace __once {
+    using namespace stdexec;
+
+    template <class _SenderId>
+    struct __sender {
+      using _Sender = stdexec::__t<_SenderId>;
+
+      struct __t {
+        using __id = __sender;
+        using is_sender = sequence_tag;
+        STDEXEC_NO_UNIQUE_ADDRESS _Sender __sndr_;
+
+        template <class _Self, class _Receiver>
+        using __next_t = __next_sender_of_t<__decay_t<_Receiver>&, __copy_cvref_t<_Self, _Sender>>;
+
+        template <__decays_to<__t> _Self, receiver _Receiver>
+          requires sequence_receiver_from<_Receiver, __copy_cvref_t<_Self, _Sender>>
+                && sender_to<__next_t<_Self, _Receiver>, _Receiver>
+        friend auto tag_invoke(sequence_connect_t, _Self&& __self, _Receiver&& __rcvr)
+          -> connect_result_t<__next_t<_Self, _Receiver>, _Receiver> {
+          return stdexec::connect(
+            exec::set_next(__rcvr, static_cast<_Self&&>(__self).__sndr_),
+            static_cast<_Receiver&&>(__rcvr));
+        }
+
+        template <__decays_to<__t> _Self, class _Env>
+        friend auto tag_invoke(get_completion_signatures_t, _Self&& __self, const _Env& __env)
+          -> completion_signatures_of_t<__copy_cvref_t<_Self, _Sender>, _Env>;
+      };
+    };
+
+    struct once_t {
+      template <class _Sender>
+      using __sender_t = __t<__sender<__id<__decay_t<_Sender>>>>;
+
+      template <class _Sender>
+        requires tag_invocable<once_t, _Sender>
+      auto operator()(_Sender&& __sender) const noexcept(nothrow_tag_invocable<once_t, _Sender>)
+        -> tag_invoke_result_t<once_t, _Sender> {
+        return tag_invoke(*this, static_cast<_Sender&&>(__sender));
+      }
+
+      template <class _Sender>
+        requires(!tag_invocable<once_t, _Sender>)
+      auto operator()(_Sender&& __sender) const -> __sender_t<_Sender> {
+        return __sender_t<_Sender>{static_cast<_Sender&&>(__sender)};
+      }
+
+      __binder_back<once_t> operator()() const noexcept {
+        return {{}, {}, {}};
+      }
+    };
+  }
+
+  using __once::once_t;
+  inline constexpr once_t once{};
+}
---
+++ test/exec/sequence/test_once.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2023 Maikel Nadolski
+ * Copyright (c) 2023 NVIDIA Corporation
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "exec/sequence/once.hpp"
+
+#include "test_common/type_helpers.hpp"
+
+#include <catch2/catch.hpp>
+
+using namespace stdexec;
+using namespace exec;
+
+template <__completion_signature... _Sigs>
+struct test_receiver {
+  template <class _Tag, class... _Args>
+    requires __one_of<_Tag(_Args...), _Sigs...>
+  friend void tag_invoke(_Tag, test_receiver&&, _Args&&...) noexcept {}
+
+  friend empty_env tag_invoke(get_env_t, test_receiver) noexcept { return {}; }
+};
+
+template <__completion_signature... _Sigs>
+struct count_receiver {
+  int& counter;
+
+  template <sender_to<test_receiver<_Sigs...>> Item>
+  friend auto tag_invoke(set_next_t, count_receiver& self, Item&& item) {
+    return then(static_cast<Item&&>(item), [&self](auto&&...) noexcept { ++self.counter; });
+  }
+
+  friend void tag_invoke(set_value_t, count_receiver&& self) noexcept {
+    CHECK(self.counter == 1);
+  }
+
+  friend empty_env tag_invoke(get_env_t, const count_receiver&) noexcept {
+    return {};
+  }
+};
+
+
+TEST_CASE("sequence_senders - once - Test for concepts", "[sequence_senders][once]") {
+  using just_t = decltype(just());
+  using just_once_t = decltype(once(just()));
+  STATIC_REQUIRE(sender<just_t>);
+  STATIC_REQUIRE(sender<just_once_t>);
+  STATIC_REQUIRE(same_as<completion_signatures_of_t<just_t>, completion_signatures_of_t<just_once_t>>);
+  STATIC_REQUIRE(sender_to<just_t, count_receiver<>>);
+  STATIC_REQUIRE_FALSE(sender_to<just_once_t, count_receiver<set_value_t()>>);
+  STATIC_REQUIRE(sequence_sender_to<just_once_t, count_receiver<set_value_t()>>);
+}
+
+TEST_CASE("sequence_senders - once - fires once", "[sequence_senders][once]") {
+  int counter = 0;
+  auto sequence = once(just(42));
+  auto receiver = count_receiver<set_value_t(int)>{counter};
+  auto op = sequence_connect(sequence, receiver);
+  start(op);
+  CHECK(counter == 1);
+}
+
+TEST_CASE("sequence_senders - once - fires once movable", "[sequence_senders][once]") {
+  int counter = 0;
+  auto sequence = once(just(::movable(42)));
+  auto receiver = count_receiver<set_value_t(::movable)>{counter};
+  using sequence_t = __decay_t<decltype(sequence)>;
+  STATIC_REQUIRE_FALSE(sequence_sender_to<const sequence_t&, count_receiver<set_value_t(::movable)>>);
+  STATIC_REQUIRE(sequence_sender_to<sequence_t&&, count_receiver<set_value_t(::movable)>>);
+  auto op = sequence_connect(std::move(sequence), receiver);
+  start(op);
+  CHECK(counter == 1);
+}
