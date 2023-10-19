5949e66db5bfe80970f5bedf1f1a55260c4f9e6a
Maikel Nadolski <maikel.nadolski@gmail.com>
2023-10-19 14:49:11 +0200

    Update io_uring example for the new interface

--- examples/io_uring.cpp
+++ examples/io_uring.cpp
@@ -31,10 +31,10 @@ int main() {
   exec::io_uring_context context;
   exec::io_uring_context context2;
   std::thread io_thread{[&] {
-    context.run();
+    context.run_until_stopped();
   }};
   std::thread io_thread2{[&] {
-    context2.run();
+    context2.run_until_stopped();
   }};
   auto scheduler = context.get_scheduler();
   auto scheduler2 = context2.get_scheduler();
@@ -87,8 +87,9 @@ int main() {
     | stdexec::then([] { std::cout << "This should not print, because the context is stopped.\n"; })
     | stdexec::upon_stopped([] { std::cout << "The context is stopped!\n"; }));
 
+  context.reset();
   io_thread = std::thread{[&] {
-    context.run();
+    context.run_until_stopped();
   }};
 
   while (!context.is_running())
