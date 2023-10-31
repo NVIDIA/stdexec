d208bff8b742ca1a70363612ec6f0910f2959521
Maikel Nadolski <maikel.nadolski@gmail.com>
2023-10-31 03:53:40 +0100

    Add copyright header to the benchmarks

--- examples/benchmark/static_thread_pool.cpp
+++ examples/benchmark/static_thread_pool.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2023 Maikel Nadolski
+ * Copyright (c) 2023 NVIDIA Corporation
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "./common.hpp"
 #include <exec/static_thread_pool.hpp>
 struct RunThread {
