NVIDIA/stdexec

commit d5d5cab6b6bff284f94376b8048195ca72f2e979

the get_completion_scheduler CPO wasn't linking with gcc in Debug mode
fixes #915

include/stdexec/execution.hpp
@@ -245,8 +245,17 @@ namespace stdexec {
   inline constexpr get_delegatee_scheduler_t get_delegatee_scheduler{};
   inline constexpr get_allocator_t get_allocator{};
   inline constexpr get_stop_token_t get_stop_token{};
+#if !STDEXEC_GCC() || defined(__OPTIMIZE__)
   template <__completion_tag _CPO>
   inline constexpr get_completion_scheduler_t<_CPO> get_completion_scheduler{};
+#else
+  template <>
+  inline constexpr get_completion_scheduler_t<set_value_t> get_completion_scheduler<set_value_t>{};
+  template <>
+  inline constexpr get_completion_scheduler_t<set_error_t> get_completion_scheduler<set_error_t>{};
+  template <>
+  inline constexpr get_completion_scheduler_t<set_stopped_t> get_completion_scheduler<set_stopped_t>{};
+#endif
 
   template <class _Tag>
   concept __forwarding_query = forwarding_query(_Tag{});

commit 6b027bc6b6ace7b9047656cf8eb6f934e0535da3

gcc: fix get_completion_scheduler for -Og
The get_completion_schedule failed to link with -Og.  There doesn't
appear to be a direct way to detect this with pre-processor flags.
Therefore, switch the detection so that only size-optimization results
in the template type (__OPTIMIZE_SIZE__ instead of __OPTIMIZE__).

Fixes: d5d5cab6b6bff284f94376b8048195ca72f2e979
Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

include/stdexec/execution.hpp
@@ -378,7 +378,7 @@ namespace stdexec {
   inline constexpr get_delegatee_scheduler_t get_delegatee_scheduler{};
   inline constexpr get_allocator_t get_allocator{};
   inline constexpr get_stop_token_t get_stop_token{};
-#if !STDEXEC_GCC() || defined(__OPTIMIZE__)
+#if !STDEXEC_GCC() || defined(__OPTIMIZE_SIZE__)
   template <__completion_tag _CPO>
   inline constexpr get_completion_scheduler_t<_CPO> get_completion_scheduler{};
 #else