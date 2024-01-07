448037030dc05235504032e4df295959ebffca1d
Eric Niebler <eniebler@nvidia.com>
2024-01-06 17:09:17 -0800

    port `exec::repeat_effect_until` to `__sexpr` (#1198)
    * port `exec::repeat_effect_until` to `__sexpr`
    
    * clean up op state if repeat_effect is connected but not started

---
+++ test/exec/test_repeat_effect_until_fail.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 NVIDIA Corporation
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdexec/execution.hpp>
+#include <exec/repeat_effect_until.hpp>
+
+namespace ex = stdexec;
+
+struct not_a_bool {};
+
+int main() {
+  ex::sender auto snd = ex::just(not_a_bool()) | exec::repeat_effect_until();
+  // build error: _INVALID_ARGUMENT_TO_REPEAT_EFFECT_UNTIL_
+  stdexec::sync_wait(std::move(snd));
+}
