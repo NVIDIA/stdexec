973a59ce067307a370ae31bd2724c50b5b8dbeb8
Eric Niebler <eniebler@nvidia.com>
2024-02-01 17:00:09 -0800

    add `exec::into_tuple`

--- test/exec/test_finally.cpp
+++ test/exec/test_finally.cpp
@@ -1,25 +1,27 @@
+/*
+ * Copyright (c) 2024 NVIDIA Corporation
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "exec/finally.hpp"
-#include "exec/materialize.hpp"
+#include "test_common/type_helpers.hpp"
 
 #include <catch2/catch.hpp>
 
 using namespace stdexec;
 
 namespace {
-
-  template <class Haystack>
-  struct __mall_contained_in {
-    template <class... Needles>
-    using __f = __mand<__mapply<__contains<Needles>, Haystack>...>;
-  };
-
-  template <class Needles, class Haystack>
-  using __all_contained_in = __mapply<__mall_contained_in<Haystack>, Needles>;
-
-  template <class Needles, class Haystack>
-  using __equivalent = __mand<
-    __all_contained_in<Needles, Haystack>,
-    std::is_same<__mapply<__msize, Needles>, __mapply<__msize, Haystack>>>;
 
   TEST_CASE("finally is a sender", "[adaptors][finally]") {
     auto s = exec::finally(just(), just());
@@ -29,17 +31,17 @@ namespace {
   TEST_CASE("finally is a sender in empty env", "[adaptors][finally]") {
     auto s = exec::finally(just(), just());
     STATIC_REQUIRE(sender_in<decltype(s), empty_env>);
-    STATIC_REQUIRE(__v<__equivalent<
+    STATIC_REQUIRE(set_equivalent<
                      completion_signatures_of_t<decltype(s), empty_env>,
-                     completion_signatures<set_error_t(std::exception_ptr), set_value_t()>>>);
+                     completion_signatures<set_error_t(std::exception_ptr), set_value_t()>>);
   }
 
   TEST_CASE("finally executes the final action", "[adaptors][finally]") {
     bool called = false;
     auto s = exec::finally(just(), just() | then([&called]() noexcept { called = true; }));
-    STATIC_REQUIRE(__v<__equivalent<
+    STATIC_REQUIRE(set_equivalent<
                      completion_signatures_of_t<decltype(s), empty_env>,
-                     completion_signatures<set_error_t(std::exception_ptr), set_value_t()>>>);
+                     completion_signatures<set_error_t(std::exception_ptr), set_value_t()>>);
     sync_wait(s);
     CHECK(called);
   }
@@ -47,9 +49,9 @@ namespace {
   TEST_CASE("finally executes the final action and returns integer", "[adaptors][finally]") {
     bool called = false;
     auto s = exec::finally(just(42), just() | then([&called]() noexcept { called = true; }));
-    STATIC_REQUIRE(__v<__equivalent<
+    STATIC_REQUIRE(set_equivalent<
                      completion_signatures_of_t<decltype(s), empty_env>,
-                     completion_signatures<set_error_t(std::exception_ptr), set_value_t(int&&)>>>);
+                     completion_signatures<set_error_t(std::exception_ptr), set_value_t(int&&)>>);
     auto [i] = *sync_wait(s);
     CHECK(called);
     CHECK(i == 42);
@@ -64,9 +66,9 @@ namespace {
         return x;
       }),
       just() | then([&called]() noexcept { called = true; }));
-    STATIC_REQUIRE(__v<__equivalent<
+    STATIC_REQUIRE(set_equivalent<
                      completion_signatures_of_t<decltype(s), empty_env>,
-                     completion_signatures<set_error_t(std::exception_ptr), set_value_t(int&&)>>>);
+                     completion_signatures<set_error_t(std::exception_ptr), set_value_t(int&&)>>);
     CHECK_THROWS_AS(sync_wait(s), int);
     CHECK(called);
   }
---
+++ test/exec/test_into_tuple.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2024 NVIDIA Corporation
+ *
+ * Licensed under the Apache License Version 2.0 with LLVM Exceptions
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *   https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "exec/into_tuple.hpp"
+#include "test_common/senders.hpp"
+#include "test_common/type_helpers.hpp"
+
+#include <catch2/catch.hpp>
+
+using namespace stdexec;
+
+namespace {
+
+  TEST_CASE("into_tuple is a sender", "[adaptors][into_tuple]") {
+    auto s = exec::into_tuple(just(42, 56));
+    STATIC_REQUIRE(sender<decltype(s)>);
+  }
+
+  TEST_CASE("into_tuple is a sender in empty env", "[adaptors][into_tuple]") {
+    using void_sender = result_of_t<exec::into_tuple, a_sender_of<set_value_t()>>;
+    STATIC_REQUIRE(sender_in<void_sender, empty_env>);
+    STATIC_REQUIRE(
+      set_equivalent<
+        completion_signatures_of_t<void_sender, empty_env>,
+        completion_signatures<set_error_t(std::exception_ptr), set_value_t(std::tuple<>)>>);
+
+    using ints_sender = result_of_t<exec::into_tuple, a_sender_of<set_value_t(int&, int&&)>>;
+    STATIC_REQUIRE(sender_in<ints_sender, empty_env>);
+    STATIC_REQUIRE(
+      set_equivalent<
+        completion_signatures_of_t<ints_sender, empty_env>,
+        completion_signatures<set_error_t(std::exception_ptr), set_value_t(std::tuple<int, int>)>>);
+  }
+
+  TEST_CASE(
+    "into_tuple rejects senders unless they have a single value completion",
+    "[adaptors][into_tuple]") {
+    using error_sender =
+      result_of_t<exec::into_tuple, a_sender_of<set_error_t(std::exception_ptr)>>;
+    STATIC_REQUIRE_FALSE(sender_in<error_sender, empty_env>);
+
+    using stopped_sender = result_of_t<exec::into_tuple, a_sender_of<set_stopped_t()>>;
+    STATIC_REQUIRE_FALSE(sender_in<stopped_sender, empty_env>);
+
+    using multi_value_sender =
+      result_of_t<exec::into_tuple, a_sender_of<set_value_t(), set_value_t(int)>>;
+    STATIC_REQUIRE_FALSE(sender_in<multi_value_sender, empty_env>);
+  }
+
+  TEST_CASE("trivial into_tuple example works", "[adaptors][into_tuple]") {
+    auto s = exec::into_tuple(just(42, 56));
+    auto [i, j] = get<0>(sync_wait(s).value());
+    CHECK(i == 42);
+    CHECK(j == 56);
+  }
+}
