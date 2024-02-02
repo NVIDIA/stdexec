a934fac156ee6f17df5a48b44ee3c72e5cfd0f13
Eric Niebler <eniebler@nvidia.com>
2024-02-01 18:08:39 -0800

    correct the copyright year

--- include/exec/reschedule.hpp
+++ include/exec/reschedule.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 NVIDIA Corporation
+ * Copyright (c) 2024 NVIDIA Corporation
  *
  * Licensed under the Apache License Version 2.0 with LLVM Exceptions
  * (the "License"); you may not use this file except in compliance with
