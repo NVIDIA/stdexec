216b5d1e1f2c39f127b56b19bc1f69afe22d516a
Lauri Vasama <git@vasama.org>
2024-02-09 17:45:49 +0200

    Restrict redundant STDEXEC_UNREACHABLE calls to NVHPC

--- include/stdexec/__detail/__basic_sender.hpp
+++ include/stdexec/__detail/__basic_sender.hpp
@@ -170,7 +170,6 @@ namespace stdexec {
       } else {
         return empty_env();
       }
-      STDEXEC_UNREACHABLE();
     };
 
     inline constexpr auto __get_env = //
@@ -511,7 +510,6 @@ namespace stdexec {
       } else {
         return __self.__impl_(__copy_cvref_fn<_Self>(), __nth_pack_element<_Idx>);
       }
-      STDEXEC_UNREACHABLE();
     }
   };
 
