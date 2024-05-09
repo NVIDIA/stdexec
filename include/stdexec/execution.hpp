87cbbbfcd2f400045a105d4eacc7b7e4c9bf66f9
Eric Niebler <eniebler@nvidia.com>
2024-05-09 10:54:33 -0700

    move `into_variant` into its own header

--- include/stdexec/execution.hpp
+++ include/stdexec/execution.hpp
@@ -31,6 +31,7 @@
 #include "__detail/__env.hpp"
 #include "__detail/__execute.hpp"
 #include "__detail/__inline_scheduler.hpp"
+#include "__detail/__into_variant.hpp"
 #include "__detail/__intrusive_ptr.hpp"
 #include "__detail/__intrusive_slist.hpp"
 #include "__detail/__just.hpp"
@@ -106,86 +107,6 @@ namespace stdexec {
       typename __mbool<bool{_Predicate(_Tag{})}>;
       requires bool { _Predicate(_Tag{}) };
     };
-
-  /////////////////////////////////////////////////////////////////////////////
-  // [execution.senders.adaptors.into_variant]
-  namespace __into_variant {
-    template <class _Sender, class _Env>
-      requires sender_in<_Sender, _Env>
-    using __into_variant_result_t = value_types_of_t<_Sender, _Env>;
-
-    template <class _Sender, class _Env>
-    using __variant_t = __try_value_types_of_t<_Sender, _Env>;
-
-    template <class _Variant>
-    using __variant_completions =
-      completion_signatures<set_value_t(_Variant), set_error_t(std::exception_ptr)>;
-
-    template <class _Sender, class _Env>
-    using __compl_sigs = //
-      __try_make_completion_signatures<
-        _Sender,
-        _Env,
-        __meval<__variant_completions, __variant_t<_Sender, _Env>>,
-        __mconst<completion_signatures<>>>;
-
-    struct into_variant_t {
-      template <sender _Sender>
-      auto operator()(_Sender&& __sndr) const -> __well_formed_sender auto {
-        auto __domain = __get_early_domain(__sndr);
-        return stdexec::transform_sender(
-          __domain, __make_sexpr<into_variant_t>(__(), std::forward<_Sender>(__sndr)));
-      }
-
-      STDEXEC_ATTRIBUTE((always_inline))
-      auto
-        operator()() const noexcept -> __binder_back<into_variant_t> {
-        return {};
-      }
-    };
-
-    struct __into_variant_impl : __sexpr_defaults {
-      static constexpr auto get_state = //
-        []<class _Self, class _Receiver>(_Self&&, _Receiver&) noexcept {
-          using __variant_t = value_types_of_t<__child_of<_Self>, env_of_t<_Receiver>>;
-          return __mtype<__variant_t>();
-        };
-
-      static constexpr auto complete = //
-        []<class _State, class _Receiver, class _Tag, class... _Args>(
-          __ignore,
-          _State,
-          _Receiver& __rcvr,
-          _Tag,
-          _Args&&... __args) noexcept -> void {
-        if constexpr (same_as<_Tag, set_value_t>) {
-          using __variant_t = __t<_State>;
-          try {
-            set_value(
-              static_cast<_Receiver&&>(__rcvr),
-              __variant_t{std::tuple<_Args&&...>{static_cast<_Args&&>(__args)...}});
-          } catch (...) {
-            set_error(static_cast<_Receiver&&>(__rcvr), std::current_exception());
-          }
-        } else {
-          _Tag()(static_cast<_Receiver&&>(__rcvr), static_cast<_Args&&>(__args)...);
-        }
-      };
-
-      static constexpr auto get_completion_signatures =       //
-        []<class _Self, class _Env>(_Self&&, _Env&&) noexcept //
-        -> __compl_sigs<__child_of<_Self>, _Env> {
-        static_assert(sender_expr_for<_Self, into_variant_t>);
-        return {};
-      };
-    };
-  } // namespace __into_variant
-
-  using __into_variant::into_variant_t;
-  inline constexpr into_variant_t into_variant{};
-
-  template <>
-  struct __sexpr_impl<into_variant_t> : __into_variant::__into_variant_impl { };
 
   /////////////////////////////////////////////////////////////////////////////
   // [execution.senders.adaptors.when_all]
