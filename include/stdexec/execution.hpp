d63e88fbb11a00fc4f40a4082d36a4b2394b3584
Eric Niebler <eniebler@nvidia.com>
2024-05-09 10:32:17 -0700

    move `start_on` into its own header

--- include/stdexec/execution.hpp
+++ include/stdexec/execution.hpp
@@ -46,6 +46,7 @@
 #include "__detail/__sender_adaptor_closure.hpp"
 #include "__detail/__split.hpp"
 #include "__detail/__start_detached.hpp"
+#include "__detail/__start_on.hpp"
 #include "__detail/__stopped_as_error.hpp"
 #include "__detail/__stopped_as_optional.hpp"
 #include "__detail/__submit.hpp"
@@ -105,82 +106,6 @@ namespace stdexec {
       typename __mbool<bool{_Predicate(_Tag{})}>;
       requires bool { _Predicate(_Tag{}) };
     };
-
-  namespace __detail {
-    template <class _Env, class _Scheduler>
-    STDEXEC_ATTRIBUTE((always_inline))
-    auto
-      __mkenv_sched(_Env&& __env, _Scheduler __sched) {
-      auto __env2 = __env::__join(
-        __env::__with(__sched, get_scheduler),
-        __env::__without(static_cast<_Env&&>(__env), get_domain));
-      using _Env2 = decltype(__env2);
-
-      struct __env_t : _Env2 { };
-
-      return __env_t{static_cast<_Env2&&>(__env2)};
-    }
-
-    template <class _Ty, class = __name_of<__decay_t<_Ty>>>
-    struct __always {
-      _Ty __val_;
-
-      auto operator()() noexcept -> _Ty {
-        return static_cast<_Ty&&>(__val_);
-      }
-    };
-
-    template <class _Ty>
-    __always(_Ty) -> __always<_Ty>;
-  } // namespace __detail
-
-  /////////////////////////////////////////////////////////////////////////////
-  // [execution.senders.adaptors.start_on]
-  namespace __start_on {
-    struct start_on_t {
-      using _Sender = __1;
-      using _Scheduler = __0;
-      using __legacy_customizations_t = __types<tag_invoke_t(start_on_t, _Scheduler, _Sender)>;
-
-      template <scheduler _Scheduler, sender _Sender>
-      auto operator()(_Scheduler&& __sched, _Sender&& __sndr) const -> __well_formed_sender auto {
-        auto __domain = query_or(get_domain, __sched, default_domain());
-        return stdexec::transform_sender(
-          __domain,
-          __make_sexpr<start_on_t>(
-            static_cast<_Scheduler&&>(__sched), static_cast<_Sender&&>(__sndr)));
-      }
-
-      template <class _Env>
-      STDEXEC_ATTRIBUTE((always_inline))
-      static auto
-        __transform_env_fn(_Env&& __env) noexcept {
-        return [&](__ignore, auto __sched, __ignore) noexcept {
-          return __detail::__mkenv_sched(static_cast<_Env&&>(__env), __sched);
-        };
-      }
-
-      template <class _Sender, class _Env>
-      static auto transform_env(const _Sender& __sndr, _Env&& __env) noexcept {
-        return __sexpr_apply(__sndr, __transform_env_fn(static_cast<_Env&&>(__env)));
-      }
-
-      template <class _Sender, class _Env>
-      static auto transform_sender(_Sender&& __sndr, const _Env&) {
-        return __sexpr_apply(
-          static_cast<_Sender&&>(__sndr),
-          []<class _Data, class _Child>(__ignore, _Data&& __data, _Child&& __child) {
-            return let_value(schedule(__data), __detail::__always{static_cast<_Child&&>(__child)});
-          });
-      }
-    };
-  } // namespace __start_on
-
-  using __start_on::start_on_t;
-  inline constexpr start_on_t start_on{};
-
-  using on_t = start_on_t;
-  inline constexpr on_t on{};
 
   /////////////////////////////////////////////////////////////////////////////
   // [execution.senders.adaptors.into_variant]
