4789b562efc16ff59aa293b9e4494f73832edd7c
Eric Niebler <eniebler@nvidia.com>
2024-06-14 10:46:17 -0700

    simplify `test-windows.ps1` script

--- .github/workflows/test-windows.ps1
+++ .github/workflows/test-windows.ps1
@@ -12,23 +12,6 @@ function Invoke-NativeCommand($Command) {
 	}
 }
 
-$VSVersion = 2022
-$VSEdition = 'Enterprise'
-$Architecture = 'x64'
-
-Push-Location "C:/Program Files/Microsoft Visual Studio/$VSVersion/$VSEdition/VC/Auxiliary/Build"
-$VCVersion = Get-Content 'Microsoft.VCToolsVersion.default.txt'
-cmd /c "vcvarsall.bat $Architecture -vcvars_ver=$VCVersion > nul & set" | ForEach-Object {
-	if ($_ -match '^(.+?)=(.*)') {
-		Set-Item -Force -Path "ENV:$($matches[1])" -Value $matches[2]
-	}
-}
-Pop-Location
-
-if ($Compiler -ne "cl") {
-	$ENV:CXX=$Compiler
-}
-
 if (Test-Path -PathType Container $BuildDirectory) {
 	Remove-Item -Recurse $BuildDirectory | Out-Null
 }
