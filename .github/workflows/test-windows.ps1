176f15dc30ccdd9904bcc29458a09017c9a0fe49
Eric Niebler <eniebler@nvidia.com>
2024-06-23 16:35:34 -0700

    run windows CI in a rapidsai container (#1349)

--- .github/workflows/test-windows.ps1
+++ .github/workflows/test-windows.ps1
@@ -12,22 +12,7 @@ function Invoke-NativeCommand($Command) {
 	}
 }
 
-$VSVersion = 2022
-$VSEdition = 'Enterprise'
-$Architecture = 'x64'
-
-Push-Location "C:/Program Files/Microsoft Visual Studio/$VSVersion/$VSEdition/VC/Auxiliary/Build"
-$VCVersion = Get-Content 'Microsoft.VCToolsVersion.default.txt'
-cmd /c "vcvarsall.bat $Architecture -vcvars_ver=$VCVersion > nul & set" | ForEach-Object {
-	if ($_ -match '^(.+?)=(.*)') {
-		Set-Item -Force -Path "ENV:$($matches[1])" -Value $matches[2]
-	}
-}
-Pop-Location
-
-if ($Compiler -ne "cl") {
-	$ENV:CXX=$Compiler
-}
+Set-Location -Path "$PSScriptRoot/../.." -PassThru
 
 if (Test-Path -PathType Container $BuildDirectory) {
 	Remove-Item -Recurse $BuildDirectory | Out-Null
@@ -35,5 +20,5 @@ if (Test-Path -PathType Container $BuildDirectory) {
 New-Item -ItemType Directory $BuildDirectory | Out-Null
 
 Invoke-NativeCommand cmake -B $BuildDirectory -G Ninja "-DCMAKE_BUILD_TYPE=$Config" .
-Invoke-NativeCommand cmake --build $BuildDirectory
+Invoke-NativeCommand cmake --build $BuildDirectory --parallel 1
 Invoke-NativeCommand ctest --test-dir $BuildDirectory
