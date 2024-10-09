4300e5e9d446aa05908de11f78a75f1ba63fd03d
David Eles <eles.david88@gmail.com>
2024-10-09 10:05:24 +0200

    Adding back tbbexec.
    The goal is to stay compatible with existing usage.
    
    Warning flag is added to notify users about the change.

---
+++ include/tbbexec/tbb_thread_pool.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <execpools/tbb/tbb_thread_pool.hpp>
+
+#warning Deprecated header file, please include the <execpools/tbb/tbb_thread_pool.hpp> header file instead and use the execpools::tbb_thread_pool class that is identical as tbbexec::thread_pool class.
+
+namespace tbbexec
+{
+  using tbb_thread_pool = execpools::tbb_thread_pool;
+}
