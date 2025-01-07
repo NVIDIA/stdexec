1663fc67558a20e57050a18610033135b0b8b971
David Eles <eles.david88@gmail.com>
2025-01-07 11:47:39 +0100

    Format new code

--- test/exec/test_static_thread_pool.cpp
+++ test/exec/test_static_thread_pool.cpp
@@ -6,16 +6,16 @@
 #include <unordered_set>
 namespace ex = stdexec;
 
-TEST_CASE("static_thread_pool::get_scheduler_on_thread Test start on a specific thread", "[types][static_thread_pool]")
-{
+TEST_CASE(
+  "static_thread_pool::get_scheduler_on_thread Test start on a specific thread",
+  "[types][static_thread_pool]") {
   constexpr const size_t num_of_threads = 5;
-  exec::static_thread_pool pool {num_of_threads};
+  exec::static_thread_pool pool{num_of_threads};
 
   std::unordered_set<std::thread::id> thread_ids;
-  for(size_t i = 0; i < num_of_threads; ++i)
-  {
+  for (size_t i = 0; i < num_of_threads; ++i) {
     auto sender = ex::schedule(pool.get_scheduler_on_thread(i))
-     | ex::then([&]{thread_ids.insert(std::this_thread::get_id());});
+                | ex::then([&] { thread_ids.insert(std::this_thread::get_id()); });
     ex::sync_wait(std::move(sender));
   }
   REQUIRE(thread_ids.size() == num_of_threads);
