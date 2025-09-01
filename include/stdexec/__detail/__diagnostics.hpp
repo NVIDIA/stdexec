ea52f46727e46006974a935a51ab87bf0e019609
Eric Niebler <eniebler@nvidia.com>
2025-08-31 18:42:56 -0700

    give up on raw string literals because of gcc bug #55971

--- include/stdexec/__detail/__diagnostics.hpp
+++ include/stdexec/__detail/__diagnostics.hpp
@@ -105,158 +105,153 @@ namespace stdexec {
 } // namespace stdexec
 
 ////////////////////////////////////////////////////////////////////////////////
-#define STDEXEC_ERROR_ENABLE_SENDER_IS_FALSE                                   \
-  R"(
-
-The given type is not a sender because stdexec::enable_sender<Sender> is false. Either:
-
-1. Give the type a nested `::sender_concept` type that is an alias for `stdexec::sender_t`,
-   as in:
-
-     class MySender
-     {
-     public:
-       using sender_concept = stdexec::sender_t;
-       ...
-     };
-
-   or,
-
-2. Specialize the `stdexec::enable_sender` boolean trait for this type to true, as follows:
-
-     class MySender
-     {
-       ...
-     };
-
-     template <>
-     inline constexpr bool stdexec::enable_sender<MySender> = true;
-)"
-
-////////////////////////////////////////////////////////////////////////////////
-#define STDEXEC_ERROR_CANNOT_COMPUTE_COMPLETION_SIGNATURES                     \
-  R"(
-
-The sender type was not able to report its completion signatures when asked.
-This is either because it lacks the necessary member function, or because the
-member function was ill-formed.
-
-A sender can declare its completion signatures in one of two ways:
-
-1. By defining a nested type alias named `completion_signatures` that is a
-  specialization of `stdexec::completion_signatures<...>`, as follows:
-
-     class MySender
-     {
-     public:
-       using sender_concept        = stdexec::sender_t;
-       using completion_signatures = stdexec::completion_signatures<
-         // This sender can complete successfully with an int and a float...
-         stdexec::set_value_t(int, float),
-         // ... or in error with an exception_ptr
-         stdexec::set_error_t(std::exception_ptr)>;
-       ...
-     };
-
-   or,
-
-2. By defining a member function named `get_completion_signatures` that returns
-   a specialization of `stdexec::completion_signatures<...>`, as follows:
-
-     class MySender
-     {
-     public:
-       using sender_concept        = stdexec::sender_t;
-
-       template <class... _Env>
-       auto get_completion_signatures(_Env&&...) -> stdexec::completion_signatures<
-         // This sender can complete successfully with an int and a float...
-         stdexec::set_value_t(int, float),
-         // ... or in error with a std::exception_ptr.
-         stdexec::set_error_t(std::exception_ptr)>
-       {
-        return {};
-       }
-       ...
-     };
-)"
+#define STDEXEC_ERROR_ENABLE_SENDER_IS_FALSE                                                       \
+  "\n"                                                                                             \
+  "\n"                                                                                             \
+  "The given type is not a sender because stdexec::enable_sender<Sender> is false. Either:\n"      \
+  "\n"                                                                                             \
+  "1. Give the type a nested `::sender_concept` type that is an alias for `stdexec::sender_t`,\n"  \
+  "   as in:\n"                                                                                    \
+  "\n"                                                                                             \
+  "     class MySender\n"                                                                          \
+  "     {\n"                                                                                       \
+  "     public:\n"                                                                                 \
+  "       using sender_concept = stdexec::sender_t;\n"                                             \
+  "       ...\n"                                                                                   \
+  "     };\n"                                                                                      \
+  "\n"                                                                                             \
+  "   or,\n"                                                                                       \
+  "\n"                                                                                             \
+  "2. Specialize the `stdexec::enable_sender` boolean trait for this type to true, as follows:\n"  \
+  "\n"                                                                                             \
+  "     class MySender\n"                                                                          \
+  "     {\n"                                                                                       \
+  "       ...\n"                                                                                   \
+  "     };\n"                                                                                      \
+  "\n"                                                                                             \
+  "     template <>\n"                                                                             \
+  "     inline constexpr bool stdexec::enable_sender<MySender> = true;\n"
 
 ////////////////////////////////////////////////////////////////////////////////
-#define STDEXEC_ERROR_GET_COMPLETION_SIGNATURES_RETURNED_AN_ERROR              \
-  R"(
-
-Trying to compute the sender's completion signatures resulted in an error. See
-the rest of the compiler diagnostic for clues. Look for the string "_ERROR_".
-)"
-
-#define STDEXEC_ERROR_GET_COMPLETION_SIGNATURES_HAS_INVALID_RETURN_TYPE        \
-  R"(
-
-The member function `get_completion_signatures` of the sender returned an
-invalid type.
-
-A sender's `get_completion_signatures` function must return a specialization of
-`stdexec::completion_signatures<...>`, as follows:
-
-  class MySender
-  {
-  public:
-    using sender_concept = stdexec::sender_t;
-
-    template <class... _Env>
-    auto get_completion_signatures(_Env&&...) -> stdexec::completion_signatures<
-      // This sender can complete successfully with an int and a float...
-      stdexec::set_value_t(int, float),
-      // ... or in error with a std::exception_ptr.
-      stdexec::set_error_t(std::exception_ptr)>
-    {
-    return {};
-    }
-    ...
-  };
-)"
-
-////////////////////////////////////////////////////////////////////////////////
-#define STDEXEC_ERROR_CANNOT_CONNECT_SENDER_TO_RECEIVER                        \
-  R"(
-A sender must provide a `connect` member function that takes a receiver as an
-argument and returns an object whose type satisfies `stdexec::operation_state`,
-as shown below:
-
-  class MySender
-  {
-  public:
-    using sender_concept        = stdexec::sender_t;
-    using completion_signatures = stdexec::completion_signatures<stdexec::set_value_t()>;
-
-    template <class Receiver>
-    struct MyOpState
-    {
-      using operation_state_concept = stdexec::operation_state_t;
-
-      void start() noexcept
-      {
-        // Start the operation, which will eventually complete and send its
-        // result to rcvr_;
-      }
-
-      Receiver rcvr_;
-    };
-
-    template <stdexec::receiver Receiver>
-    auto connect(Receiver rcvr) -> MyOpState<Receiver>
-    {
-      return MyOpState<Receiver>{std::move(rcvr)};
-    }
-
-    ...
-  };
-)"
+#define STDEXEC_ERROR_CANNOT_COMPUTE_COMPLETION_SIGNATURES                                         \
+  "\n"                                                                                             \
+  "\n"                                                                                             \
+  "The sender type was not able to report its completion signatures when asked.\n"                 \
+  "This is either because it lacks the necessary member function, or because the\n"                \
+  "member function was ill-formed.\n"                                                              \
+  "\n"                                                                                             \
+  "A sender can declare its completion signatures in one of two ways:\n"                           \
+  "\n"                                                                                             \
+  "1. By defining a nested type alias named `completion_signatures` that is a\n"                   \
+  "  specialization of `stdexec::completion_signatures<...>`, as follows:\n"                       \
+  "\n"                                                                                             \
+  "     class MySender\n"                                                                          \
+  "     {\n"                                                                                       \
+  "     public:\n"                                                                                 \
+  "       using sender_concept        = stdexec::sender_t;\n"                                      \
+  "       using completion_signatures = stdexec::completion_signatures<\n"                         \
+  "         // This sender can complete successfully with an int and a float...\n"                 \
+  "         stdexec::set_value_t(int, float),\n"                                                   \
+  "         // ... or in error with an exception_ptr\n"                                            \
+  "         stdexec::set_error_t(std::exception_ptr)>;\n"                                          \
+  "       ...\n"                                                                                   \
+  "     };\n"                                                                                      \
+  "\n"                                                                                             \
+  "   or,\n"                                                                                       \
+  "\n"                                                                                             \
+  "2. By defining a member function named `get_completion_signatures` that returns\n"              \
+  "   a specialization of `stdexec::completion_signatures<...>`, as follows:\n"                    \
+  "\n"                                                                                             \
+  "     class MySender\n"                                                                          \
+  "     {\n"                                                                                       \
+  "     public:\n"                                                                                 \
+  "       using sender_concept        = stdexec::sender_t;\n"                                      \
+  "\n"                                                                                             \
+  "       template <class... _Env>\n"                                                              \
+  "       auto get_completion_signatures(_Env&&...) -> stdexec::completion_signatures<\n"          \
+  "         // This sender can complete successfully with an int and a float...\n"                 \
+  "         stdexec::set_value_t(int, float),\n"                                                   \
+  "         // ... or in error with a std::exception_ptr.\n"                                       \
+  "         stdexec::set_error_t(std::exception_ptr)>\n"                                           \
+  "       {\n"                                                                                     \
+  "        return {};\n"                                                                           \
+  "       }\n"                                                                                     \
+  "       ...\n"                                                                                   \
+  "     };\n"
 
 ////////////////////////////////////////////////////////////////////////////////
-#define STDEXEC_ERROR_SYNC_WAIT_CANNOT_CONNECT_SENDER_TO_RECEIVER              \
-  R"(
+#define STDEXEC_ERROR_GET_COMPLETION_SIGNATURES_RETURNED_AN_ERROR                                  \
+  "\n"                                                                                             \
+  "\n"                                                                                             \
+  "Trying to compute the sender's completion signatures resulted in an error. See\n"               \
+  "the rest of the compiler diagnostic for clues. Look for the string \"_ERROR_\".\n"
 
-The sender passed to `stdexec::sync_wait()` does not have a `connect`
-member function that accepts sync_wait's receiver.
-)" STDEXEC_ERROR_CANNOT_CONNECT_SENDER_TO_RECEIVER
+#define STDEXEC_ERROR_GET_COMPLETION_SIGNATURES_HAS_INVALID_RETURN_TYPE                            \
+  "\n"                                                                                             \
+  "\n"                                                                                             \
+  "The member function `get_completion_signatures` of the sender returned an\n"                    \
+  "invalid type.\n"                                                                                \
+  "\n"                                                                                             \
+  "A sender's `get_completion_signatures` function must return a specialization of\n"              \
+  "`stdexec::completion_signatures<...>`, as follows:\n"                                           \
+  "\n"                                                                                             \
+  "  class MySender\n"                                                                             \
+  "  {\n"                                                                                          \
+  "  public:\n"                                                                                    \
+  "    using sender_concept = stdexec::sender_t;\n"                                                \
+  "\n"                                                                                             \
+  "    template <class... _Env>\n"                                                                 \
+  "    auto get_completion_signatures(_Env&&...) -> stdexec::completion_signatures<\n"             \
+  "      // This sender can complete successfully with an int and a float...\n"                    \
+  "      stdexec::set_value_t(int, float),\n"                                                      \
+  "      // ... or in error with a std::exception_ptr.\n"                                          \
+  "      stdexec::set_error_t(std::exception_ptr)>\n"                                              \
+  "    {\n"                                                                                        \
+  "    return {};\n"                                                                               \
+  "    }\n"                                                                                        \
+  "    ...\n"                                                                                      \
+  "  };\n"
+
+////////////////////////////////////////////////////////////////////////////////
+#define STDEXEC_ERROR_CANNOT_CONNECT_SENDER_TO_RECEIVER                                            \
+  "\n"                                                                                             \
+  "A sender must provide a `connect` member function that takes a receiver as an\n"                \
+  "argument and returns an object whose type satisfies `stdexec::operation_state`,\n"              \
+  "as shown below:\n"                                                                              \
+  "\n"                                                                                             \
+  "  class MySender\n"                                                                             \
+  "  {\n"                                                                                          \
+  "  public:\n"                                                                                    \
+  "    using sender_concept        = stdexec::sender_t;\n"                                         \
+  "    using completion_signatures = stdexec::completion_signatures<stdexec::set_value_t()>;\n"    \
+  "\n"                                                                                             \
+  "    template <class Receiver>\n"                                                                \
+  "    struct MyOpState\n"                                                                         \
+  "    {\n"                                                                                        \
+  "      using operation_state_concept = stdexec::operation_state_t;\n"                            \
+  "\n"                                                                                             \
+  "      void start() noexcept\n"                                                                  \
+  "      {\n"                                                                                      \
+  "        // Start the operation, which will eventually complete and send its\n"                  \
+  "        // result to rcvr_;\n"                                                                  \
+  "      }\n"                                                                                      \
+  "\n"                                                                                             \
+  "      Receiver rcvr_;\n"                                                                        \
+  "    };\n"                                                                                       \
+  "\n"                                                                                             \
+  "    template <stdexec::receiver Receiver>\n"                                                    \
+  "    auto connect(Receiver rcvr) -> MyOpState<Receiver>\n"                                       \
+  "    {\n"                                                                                        \
+  "      return MyOpState<Receiver>{std::move(rcvr)};\n"                                           \
+  "    }\n"                                                                                        \
+  "\n"                                                                                             \
+  "    ...\n"                                                                                      \
+  "  };\n"
+
+////////////////////////////////////////////////////////////////////////////////
+#define STDEXEC_ERROR_SYNC_WAIT_CANNOT_CONNECT_SENDER_TO_RECEIVER                                  \
+  "\n"                                                                                             \
+  "\n"                                                                                             \
+  "The sender passed to `stdexec::sync_wait()` does not have a `connect`\n"                        \
+  "member function that accepts sync_wait's "                                                      \
+  "receiver.\n" STDEXEC_ERROR_CANNOT_CONNECT_SENDER_TO_RECEIVER
