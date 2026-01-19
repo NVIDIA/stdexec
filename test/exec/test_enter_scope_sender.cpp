2161422872ec70835f4667ddbc68d052c708cd1e
Robert Leahy <rleahy@rleahy.ca>
2026-01-19 09:26:13 -0500

    exec::enter_scope_sender et al.
    Adds concepts which deal with enter scope senders. Whereas an exit scope
    sender performs the operations necessary to leave a scope, an enter
    scope sender performs the operations necessary to enter a scope. Just
    entering a scope is insufficient, however, as if a scope is entered it
    must be exited. Therefore enter scope senders not only perform the
    actions which must be taken to enter a scope, but also complete with a
    sender which undoes those actions, i.e. all enter scope senders are a
    higher-order sender.

---
+++ test/exec/test_enter_scope_sender.cpp
@@ -0,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ *                         Copyright (c) 2025 Robert Leahy. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ * Licensed under the Apache License, Version 2.0 with LLVM Exceptions (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <exec/enter_scope_sender.hpp>
+
+#include <catch2/catch.hpp>
+#include <exec/variant_sender.hpp>
+#include <stdexec/execution.hpp>
+
+namespace {
+
+static_assert(
+  !::exec::enter_scope_sender_in<
+    decltype(::STDEXEC::just()),
+    ::STDEXEC::env<>>);
+static_assert(
+  ::exec::enter_scope_sender_in<
+    decltype(::STDEXEC::just(::STDEXEC::just())),
+    ::STDEXEC::env<>>);
+
+static_assert(
+  std::is_same_v<
+    decltype(::STDEXEC::just()),
+    ::exec::exit_scope_sender_of_t<
+      decltype(::STDEXEC::just(::STDEXEC::just())),
+      ::STDEXEC::env<>>>);
+static_assert(
+  std::is_same_v<
+    decltype(::STDEXEC::just()),
+    ::exec::exit_scope_sender_of_t<
+      ::exec::variant_sender<
+        decltype(::STDEXEC::just(::STDEXEC::just())),
+        decltype(::STDEXEC::just_stopped())>,
+      ::STDEXEC::env<>>>);
+
+} // unnamed namespace
