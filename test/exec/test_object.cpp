3bfabbdd51142724bcbbe0cfe2f8c78c7df0a93a
Robert Leahy <rleahy@rleahy.ca>
2026-01-24 15:55:57 -0500

    exec::object et al.
    Adds the concepts which deal with asynchronous objects. An asynchronous
    object is an object whose constructor and destructor are asynchronous
    operations (as opposed to regular, synchronous object which have
    constructors and destructors which are regular, synchronous functions).
    
    Whereas senders are a fully-curried asynchronous function the concept of
    an asynchronous object embodied by the concepts in this commit is a
    fully-curried asynchronous constructor. The particular form of the
    aforementioned is a unary invocable whose:
    
    - Sole argument is a pointer to storage whereat the asynchronous object
      shall be placed
    - Returned value is an enter scope sender which constructs the object
      (asynchronously) when the scope is entered, and destroys the object
      (asynchronously) when the scope is exited

---
+++ test/exec/test_object.cpp
@@ -0,0 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ *                         Copyright (c) 2025 Robert Leahy. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ * Licensed under the Apache License, Version 2.0 with LLVM Exceptions (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://llvm.org/LICENSE.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <exec/object.hpp>
+
+#include <catch2/catch.hpp>
+#include <exec/sync_object.hpp>
+#include <stdexec/execution.hpp>
+
+namespace {
+
+static_assert(::exec::object<::exec::sync_object<int>>);
+static_assert(::exec::object_in<::exec::sync_object<int>, ::STDEXEC::env<>>);
+
+} // unnamed namespace
