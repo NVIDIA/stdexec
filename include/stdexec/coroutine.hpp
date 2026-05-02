7d82400c7e7b520cb980d32ce1fbff44c5d5be69
Eric Niebler <eniebler@nvidia.com>
2026-05-01 18:21:22 -0700

    does `__coroutine_unhandled_stopped` have any purpose?

--- include/stdexec/coroutine.hpp
+++ include/stdexec/coroutine.hpp
@@ -224,27 +224,27 @@ namespace STDEXEC
     {&__destroy_and_continue_frame::__resume},
     {}};
 
-  struct __unhandled_stopped_frame : __detail::__synthetic_coro_frame
-  {
-    static void __resume(void* __address) noexcept
-    {
-      // Make a local copy of the promise since it will go away once we call through
-      // the __unhandled_stopped_fn_ function pointer.
-      auto& __self = *static_cast<__unhandled_stopped_frame*>(__address);
-      STDEXEC::__coroutine_resume_nothrow(__self.__promise_.__coro_.unhandled_stopped());
-    }
+  // struct __unhandled_stopped_frame : __detail::__synthetic_coro_frame
+  // {
+  //   static void __resume(void* __address) noexcept
+  //   {
+  //     // Make a local copy of the promise since it will go away once we call through
+  //     // the __unhandled_stopped_fn_ function pointer.
+  //     auto& __self = *static_cast<__unhandled_stopped_frame*>(__address);
+  //     STDEXEC::__coroutine_resume_nothrow(__self.__promise_.__coro_.unhandled_stopped());
+  //   }
 
-    struct __promise
-    {
-      __coroutine_handle<> __coro_;
-    } __promise_;
+  //   struct __promise
+  //   {
+  //     __coroutine_handle<> __coro_;
+  //   } __promise_;
 
-    static thread_local __unhandled_stopped_frame value;
-  };
+  //   static thread_local __unhandled_stopped_frame value;
+  // };
 
-  inline thread_local __unhandled_stopped_frame __unhandled_stopped_frame::value{
-    {&__unhandled_stopped_frame::__resume},
-    {}};
+  // inline thread_local __unhandled_stopped_frame __unhandled_stopped_frame::value{
+  //   {&__unhandled_stopped_frame::__resume},
+  //   {}};
 
   inline auto __coroutine_destroy_and_continue(__std::coroutine_handle<> __destroy,            //
                                                __std::coroutine_handle<> __continue) noexcept  //
@@ -255,12 +255,12 @@ namespace STDEXEC
     return __std::coroutine_handle<>::from_address(&__destroy_and_continue_frame::value);
   }
 
-  inline auto __coroutine_unhandled_stopped(__coroutine_handle<> __coro) noexcept  //
-    -> __std::coroutine_handle<>
-  {
-    __unhandled_stopped_frame::value.__promise_.__coro_ = __coro;
-    return __std::coroutine_handle<>::from_address(&__unhandled_stopped_frame::value);
-  }
+  // inline auto __coroutine_unhandled_stopped(__coroutine_handle<> __coro) noexcept  //
+  //   -> __std::coroutine_handle<>
+  // {
+  //   __unhandled_stopped_frame::value.__promise_.__coro_ = __coro;
+  //   return __std::coroutine_handle<>::from_address(&__unhandled_stopped_frame::value);
+  // }
 
 #  else
 
@@ -273,6 +273,15 @@ namespace STDEXEC
     return __continue;
   }
 
+  // STDEXEC_ATTRIBUTE(always_inline)
+  // auto __coroutine_unhandled_stopped(__coroutine_handle<> __coro) noexcept  //
+  //   -> __std::coroutine_handle<>
+  // {
+  //   return __coro.unhandled_stopped();
+  // }
+
+#  endif  // !defined(STDEXEC_MSVC_CORO_DESTROY_BUG_WORKAROUND)
+
   STDEXEC_ATTRIBUTE(always_inline)
   auto __coroutine_unhandled_stopped(__coroutine_handle<> __coro) noexcept  //
     -> __std::coroutine_handle<>
@@ -280,7 +289,6 @@ namespace STDEXEC
     return __coro.unhandled_stopped();
   }
 
-#  endif  // STDEXEC_MSVC() && STDEXEC_MSVC_VERSION < 1950
 }  // namespace STDEXEC
 
 #endif  // !STDEXEC_NO_STDCPP_COROUTINES()
