4321ae46077496e00f4ac4bae1162207f820c816
Minh Vu <vuhoangminh97@gmail.com>
2026-06-18 19:06:07 +0200

    Fix schedule_all chunking for small ranges

--- test/exec/test_static_thread_pool.cpp
+++ test/exec/test_static_thread_pool.cpp
@@ -1,8 +1,12 @@
 #include "catch2/catch_all.hpp"
+#include "exec/sequence/ignore_all_values.hpp"
+#include "exec/sequence/transform_each.hpp"
 #include <exec/static_thread_pool.hpp>
 #include <stdexec/execution.hpp>
 
+#include <atomic>
 #include <mutex>
+#include <ranges>
 #include <thread>
 #include <unordered_set>
 namespace ex = STDEXEC;
@@ -43,6 +47,32 @@ TEST_CASE("bulk on static_thread_pool executes on multiple threads", "[types][st
                                            }));
   ex::sync_wait(std::move(sender));
   REQUIRE(thread_ids.size() == num_of_threads);
+}
+
+TEST_CASE("schedule_all on static_thread_pool handles ranges smaller than available parallelism",
+          "[types][static_thread_pool]")
+{
+  constexpr size_t const num_of_threads = 5;
+  constexpr int const    range_size     = 3;
+
+  exec::static_thread_pool pool{num_of_threads};
+  REQUIRE(range_size < pool.available_parallelism());
+
+  std::atomic<int> count{0};
+  std::atomic<int> sum{0};
+  auto             sender =
+    exec::schedule_all(pool, std::views::iota(0, range_size))
+    | exec::transform_each(ex::then(
+      [&](int x) noexcept
+      {
+        count.fetch_add(1, std::memory_order_relaxed);
+        sum.fetch_add(x, std::memory_order_relaxed);
+      }))
+    | exec::ignore_all_values();
+
+  CHECK(ex::sync_wait(std::move(sender)));
+  CHECK(count.load(std::memory_order_relaxed) == range_size);
+  CHECK(sum.load(std::memory_order_relaxed) == 3);
 }
 
 TEST_CASE("bulk on static_thread_pool executes on multiple threads, take 2",
