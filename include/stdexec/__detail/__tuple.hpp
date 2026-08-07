2295844eece1165e451a4717912afd411a44afaa
Ian Petersen <ispeters@gmail.com>
2026-08-07 16:57:29 -0700

    Address post-rebase build failures
    Rebasing on `main` led to new build failures because more things needed
    exporting from the module. This diff addresses those cases.

--- include/stdexec/__detail/__tuple.hpp
+++ include/stdexec/__detail/__tuple.hpp
@@ -324,6 +324,7 @@ namespace STDEXEC
   STDEXEC_MODULE_EXPORT_AUTHORING
   using __tup::__tuple;
 
+  STDEXEC_MODULE_EXPORT_AUTHORING
   inline constexpr __tup::__apply_t __apply{};
 
   STDEXEC_MODULE_EXPORT_AUTHORING
