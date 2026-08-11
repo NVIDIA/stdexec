02729eb751a5a72f31df6454b8659954f9d38aa8
Minh Vu <vuhoangminh97@gmail.com>
2026-08-11 18:50:07 +0200

    Synchronize nvexec ensure_started storage destruction

--- test/nvexec/ensure_started.cpp
+++ test/nvexec/ensure_started.cpp
@@ -13,6 +13,61 @@ using nvexec::is_on_gpu;
 
 namespace
 {
+  class mapped_flag_t
+  {
+    int* h_flag_{};
+    int* d_flag_{};
+
+   public:
+    mapped_flag_t(mapped_flag_t const &)                    = delete;
+    mapped_flag_t(mapped_flag_t&&)                          = delete;
+    auto operator=(mapped_flag_t const &) -> mapped_flag_t& = delete;
+    auto operator=(mapped_flag_t&&) -> mapped_flag_t&       = delete;
+
+    mapped_flag_t()
+    {
+      STDEXEC_TRY_CUDA_API(cudaHostAlloc(&h_flag_, sizeof(int), cudaHostAllocMapped));
+      STDEXEC_TRY_CUDA_API(cudaHostGetDevicePointer(&d_flag_, h_flag_, 0));
+      *h_flag_ = 0;
+    }
+
+    ~mapped_flag_t()
+    {
+      STDEXEC_ASSERT_CUDA_API(cudaFreeHost(h_flag_));
+    }
+
+    class handle_t
+    {
+      int* h_flag_{};
+      int* d_flag_{};
+
+      handle_t(int* h_flag, int* d_flag)
+        : h_flag_(h_flag)
+        , d_flag_(d_flag)
+      {}
+
+     public:
+      __host__ __device__ void set() const
+      {
+        cuda::atomic_ref<int, cuda::thread_scope_system> flag{*(is_on_gpu() ? d_flag_ : h_flag_)};
+        flag.store(1, cuda::memory_order_release);
+      }
+
+      friend mapped_flag_t;
+    };
+
+    auto get() -> handle_t
+    {
+      return {h_flag_, d_flag_};
+    }
+
+    auto is_set() const -> bool
+    {
+      cuda::atomic_ref<int, cuda::thread_scope_system> flag{*h_flag_};
+      return flag.load(cuda::memory_order_acquire) == 1;
+    }
+  };
+
   class lifetime_counter_t
   {
     int  h_counter_storage_{};
@@ -20,6 +75,11 @@ namespace
     int* d_counter_{};
 
    public:
+    lifetime_counter_t(lifetime_counter_t const &)                    = delete;
+    lifetime_counter_t(lifetime_counter_t&&)                          = delete;
+    auto operator=(lifetime_counter_t const &) -> lifetime_counter_t& = delete;
+    auto operator=(lifetime_counter_t&&) -> lifetime_counter_t&       = delete;
+
     lifetime_counter_t()
     {
       STDEXEC_TRY_CUDA_API(cudaMalloc(&d_counter_, sizeof(int)));
@@ -144,6 +204,74 @@ namespace
     }
 
     REQUIRE(counter.alive() == 0);
+  }
+
+  TEST_CASE("nvexec ensure_started destroys completion storage when detached",
+            "[cuda][stream][adaptors][ensure_started]")
+  {
+    lifetime_counter_t counter{};
+
+    {
+      nvexec::stream_context stream_ctx{};
+      auto                   handle      = counter.get();
+      auto                   make_tracer = [handle]() -> lifetime_tracer_t
+      {
+        NV_IF_TARGET(NV_IS_DEVICE,
+                     (auto const start = clock64(); while (clock64() - start < 10'000'000){}));
+        return lifetime_tracer_t{handle};
+      };
+      auto predecessor = a_sender(ex::schedule(stream_ctx.get_scheduler()), make_tracer);
+
+      {
+        auto snd = exec::ensure_started(std::move(predecessor));
+      }
+
+      STDEXEC_TRY_CUDA_API(cudaDeviceSynchronize());
+    }
+
+    REQUIRE(counter.alive() == 0);
+  }
+
+  TEST_CASE("nvexec ensure_started synchronizes direct stream completion when detached",
+            "[cuda][stream][adaptors][ensure_started]")
+  {
+    int device{};
+    STDEXEC_TRY_CUDA_API(cudaGetDevice(&device));
+
+    int can_map_host_memory{};
+    STDEXEC_TRY_CUDA_API(
+      cudaDeviceGetAttribute(&can_map_host_memory, cudaDevAttrCanMapHostMemory, device));
+    if (!can_map_host_memory)
+    {
+      SKIP("device does not support mapped host memory");
+    }
+
+    mapped_flag_t completion{};
+
+    {
+      nvexec::stream_context stream_ctx{};
+      auto                   flag        = completion.get();
+      auto                   predecessor = ex::schedule(stream_ctx.get_scheduler())
+                       | ex::then(
+                           [flag]() -> int
+                           {
+                             NV_IF_TARGET(NV_IS_DEVICE,
+                                          (auto const start = clock64();
+                                           while (clock64() - start < 10'000'000) {} flag.set();));
+                             return 42;
+                           });
+
+      static_assert(
+        nvexec::_strm::stream_sender<decltype(predecessor), nvexec::_strm::_ensure_started::env_t>);
+
+      {
+        auto snd = exec::ensure_started(std::move(predecessor));
+      }
+
+      bool const completed_before_cleanup = completion.is_set();
+      STDEXEC_TRY_CUDA_API(cudaDeviceSynchronize());
+      REQUIRE(completed_before_cleanup);
+    }
   }
 
   TEST_CASE("ensure_started can preceed a sender without values",
