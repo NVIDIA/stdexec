d3a4e456bf838436ef788d4b419fc41215db6fdb
Minh Vu <vuhoangminh97@gmail.com>
2026-08-12 01:24:35 +0200

    Fix nvexec upon_error callable forwarding

--- test/nvexec/upon_error.cpp
+++ test/nvexec/upon_error.cpp
@@ -1,6 +1,8 @@
 #include <stdexec/execution.hpp>
 #include <test_common/catch2.hpp>
 
+#include <type_traits>
+
 #include "common.cuh"
 #include "nvexec/stream_context.cuh"
 
@@ -11,6 +13,23 @@ using nvexec::is_on_gpu;
 namespace
 {
 
+  struct move_only_error_handler
+  {
+    move_only_error_handler()                                = default;
+    move_only_error_handler(move_only_error_handler const &) = delete;
+
+    STDEXEC_ATTRIBUTE(host, device)
+    move_only_error_handler(move_only_error_handler &&) = default;
+
+    STDEXEC_ATTRIBUTE(host, device) auto operator()(int error) const -> int
+    {
+      return error;
+    }
+  };
+
+  static_assert(std::is_trivially_copyable_v<move_only_error_handler>);
+  static_assert(!std::is_copy_constructible_v<move_only_error_handler>);
+
   TEST_CASE("nvexec upon_error returns a sender", "[cuda][stream][adaptors][upon_error]")
   {
     nvexec::stream_context stream_ctx{};
@@ -19,6 +38,18 @@ namespace
              | ex::upon_error([](int) {});
     STATIC_REQUIRE(ex::sender<decltype(snd)>);
     (void) snd;
+  }
+
+  TEST_CASE("nvexec upon_error supports move-only function objects",
+            "[cuda][stream][adaptors][upon_error]")
+  {
+    nvexec::stream_context stream_ctx{};
+
+    auto snd = ex::just_error(42) | ex::continues_on(stream_ctx.get_scheduler())
+             | ex::upon_error(move_only_error_handler{});
+    auto const [result] = STDEXEC::sync_wait(std::move(snd)).value();
+
+    REQUIRE(result == 42);
   }
 
   TEST_CASE("nvexec upon_error executes on GPU", "[cuda][stream][adaptors][upon_error]")
