e3cef79a2341dad83a2d0458b182f75a31915775
Minh Vu <vuhoangminh97@gmail.com>
2026-08-16 01:35:34 +0200

    Format NVTX regression test

--- test/nvexec/nvtx.cpp
+++ test/nvexec/nvtx.cpp
@@ -10,8 +10,7 @@ namespace
   TEST_CASE("nvexec nvtx preserves names for lvalue senders", "[cuda][stream][nvtx]")
   {
     nvexec::stream_context stream_ctx{};
-    auto                   snd = ex::schedule(stream_ctx.get_scheduler())
-                 | nvexec::nvtx::push("test");
+    auto snd = ex::schedule(stream_ctx.get_scheduler()) | nvexec::nvtx::push("test");
 
     ex::sync_wait(snd);
 
